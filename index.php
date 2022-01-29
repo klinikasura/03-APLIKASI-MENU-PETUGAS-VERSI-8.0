4249a35611e050139af47fa28f4067926c92116c
Klinik Utama dr. Aisyah <94031971+klinikutamadraisyah12@users.noreply.github.com>
2022-01-29 10:40:11 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Aplikasi RS. Asura Tugumulyo</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- 
+        Rectangle Template 
+        http://www.templatemo.com/preview/templatemo_439_rectangle
+    	-->
+        <link rel="stylesheet" href="template/css/normalize.css">
+        <link rel="stylesheet" href="template/css/font-awesome.css">
+        <link rel="stylesheet" href="template/css/bootstrap.min.css">
+        <link rel="stylesheet" href="template/css/templatemo-style.css">
+		<link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+        <script src="template/js/vendor/modernizr-2.6.2.min.js"></script>
+        <style type="text/css">
+<!--
+.style1 {color: #333333}
+.style2 {color: #003300}
+-->
+        </style>
+</head>
+    <body>
+	
+	<style type="text/css">
+
+<!--
+
+body {
+
+	background-image: url(assetsweb/images/DESAIN.gif);
+
+}
+
+-->
+
+</style></head>
+
+<meta http-equiv="refresh" content="12;url=http://192.168.88.203/dashboard/indexweb.php">
+
+<body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <div class=""></div>
+        <div class=""></div>
+
+        
+
+
+        <div class="visible-xs visible-sm responsive-menu">
+            
+                
+            </a>
+            <div class="show-menu">
+                <ul class="main-menu">
+                    
+                </ul>
+            </div>
+        </div>
+
+        <div align="center">
+          <p>&nbsp;</p>
+          <p>&nbsp;</p>
+          <p>&nbsp;</p>
+          <p>&nbsp;</p>
+          <center><font size="4" face="comic sans ms">Desain Hamba Alloh</font><br></center>
+          <center><img src="http://192.168.88.203/dashboard/download.jpeg" alt="" border="0"></center>
+          <center><font size="4" face="comic sans ms">Rumah Sakit Asura Tugumulyo</font><br></center>
+          <center><font size="4" face="comic sans ms">Aplikasi Versi 6.0</font><br></center>
+    </div>
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+            <script src="template/js/vendor/jquery-1.10.2.min.js"></script>
+            <script src="template/js/plugins.js"></script>
+            <script src="template/js/main.js"></script>
+            <!-- templatemo 439 rectangle -->
+          </p>
+        </div>
+    </body>
+</html>
