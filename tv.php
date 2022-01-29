57f5f62e9bac0e44ddbf653013a4a4fa455e8f6b
Klinik Utama dr. Aisyah <94031971+klinikutamadraisyah12@users.noreply.github.com>
2022-01-29 10:36:10 +0700

    Add files via upload

---
+++ tv.php
@@ -0,0 +1,23 @@
+<center><button id="goFS">KLIK LAYAR PENUH</button>
+<button type="button" class="get_bg">
+ <a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/" >DISPLAY</a>
+</button></center>
+
+<link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+  <title>DISPLAY TV</title></head>
+<script>
+
+  var goFS = document.getElementById("goFS");
+
+  goFS.addEventListener("click", function() {
+
+      document.body.requestFullscreen();
+
+  }, false);
+
+</script>
+
+
+
+<iframe width="1000" height="100%" src="http://192.168.88.203/dashboard/KHANZALITE-PAKESUARA/anjungan/loket" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
