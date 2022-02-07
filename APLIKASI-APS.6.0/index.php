22f7046283efdfb8cb112477dd3b4c0829dd94bc
Klinik Utama dr. Aisyah <94031971+klinikutamadraisyah12@users.noreply.github.com>
2022-02-07 10:27:09 +0700

    Add files via upload

---
+++ APLIKASI-APS.6.0/index.php
@@ -0,0 +1,215 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+<link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+<center><button id="goFS">KLIK LAYAR PENUH</button>
+<button type="button" class="get_bg">
+ <a href="http://192.168.88.203/dashboard/indexweb.php" >MENU</a>
+</button></center>
+
+<p></p>
+<script>
+
+  var goFS = document.getElementById("goFS");
+
+  goFS.addEventListener("click", function() {
+
+      document.body.requestFullscreen();
+
+  }, false);
+
+</script>
+<p></p>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!--
+
+TemplateMo 570 Chain App Dev
+
+https://templatemo.com/tm-570-chain-app-dev
+
+-->
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/templatemo-chain-app-dev.css">
+    <link rel="stylesheet" href="assets/css/animated.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+
+  <title>Aplikasi RS. Asura Tugumulyo</title></head>
+
+<body>
+<!-- ***** Preloader Start ***** --><!-- ***** Preloader End ***** -->
+
+  <!-- ***** Header Area Start ***** --><!-- ***** Header Area End ***** -->
+  
+  <div id="modal" class="popupContainer" style="display:none;">
+    <div class="popupHeader">
+        <span class="header_title">Login</span>
+        <span class="modal_close"><i class="fa fa-times"></i></span>
+    </div>
+
+    <section class="popupBody">
+        <!-- Social Login -->
+        <div class="social_login">
+            <div class="">
+                <a href="#" class="social_box fb">
+                    <span class="icon"><i class="fab fa-facebook"></i></span>
+                    <span class="icon_title">Connect with Facebook</span>
+
+                </a>
+
+                <a href="#" class="social_box google">
+                    <span class="icon"><i class="fab fa-google-plus"></i></span>
+                    <span class="icon_title">Connect with Google</span>
+                </a>
+            </div>
+
+            <div class="centeredText">
+                <span>Or use your Email address</span>
+            </div>
+
+            <div class="action_btns">
+                <div class="one_half"><a href="#" id="login_form" class="btn">Login</a></div>
+                <div class="one_half last"><a href="#" id="register_form" class="btn">Sign up</a></div>
+            </div>
+        </div>
+
+        <!-- Username & Password Login form -->
+        <div class="user_login">
+            <form>
+                <label>Email / Username</label>
+                <input type="text" />
+                <br />
+
+                <label>Password</label>
+                <input type="password" />
+                <br />
+
+                <div class="checkbox">
+                    <input id="remember" type="checkbox" />
+                    <label for="remember">Remember me on this computer</label>
+                </div>
+
+                <div class="action_btns">
+                    <div class="one_half"><a href="#" class="btn back_btn"><i class="fa fa-angle-double-left"></i> Back</a></div>
+                    <div class="one_half last"><a href="#" class="btn btn_red">Login</a></div>
+                </div>
+            </form>
+
+            <a href="#" class="forgot_password">Forgot password?</a>
+        </div>
+
+        <!-- Register Form -->
+        <div class="user_register">
+            <form>
+                <label>Full Name</label>
+                <input type="text" />
+                <br />
+
+                <label>Email Address</label>
+                <input type="email" />
+                <br />
+
+                <label>Password</label>
+                <input type="password" />
+                <br />
+
+                <div class="checkbox">
+                    <input id="send_updates" type="checkbox" />
+                    <label for="send_updates">Send me occasional email updates</label>
+                </div>
+
+                <div class="action_btns">
+                    <div class="one_half"><a href="#" class="btn back_btn"><i class="fa fa-angle-double-left"></i> Back</a></div>
+                    <div class="one_half last"><a href="#" class="btn btn_red">Register</a></div>
+                </div>
+            </form>
+        </div>
+    </section>
+</div>
+
+  
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+<div id="pricing" class="pricing-tables">
+    <div class="container">
+      <div class="row">
+       
+        <div class="col-lg-4">
+          <div class="pricing-item-pro">
+            <span class="price">APLIKASI</span>
+            <h4>CETAK ANTRIAN</h4>
+            <div class="icon">
+              <img src="https://iconarchive.com/download/i85607/graphicloads/100-flat/print.ico" alt="">            </div>
+            <ul>
+             
+              
+            </ul>
+            <div class="border-button">
+              <a href="CETAK-ANTRIAN/index.php">KLIK</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="pricing-item-pro">
+            <span class="price">APLIKASI</span>
+            <h4>DAFTAR PASIEN</h4>
+            <div class="icon">
+              <img src="https://www.smpn11cilegon.sch.id/upload/picture/13252592daftar-online.png" alt="">            </div>
+            <ul>
+            
+              
+            </ul>
+            <div class="border-button">
+              <a href="DAFTAR-PASIEN/index.php">KLIK</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="pricing-item-pro">
+            <span class="price">APLIKASI</span>
+            <h4>DISPLAY TV </h4>
+            <div class="icon">
+              <img src="https://www.pngall.com/wp-content/uploads/4/LCD-Computer-Monitor-Transparent.png" alt="">            </div>
+            <ul>
+            
+            </ul>
+            <div class="border-button">
+                  <a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/DISPLAY-TV/tv.php">Klik</a>
+        </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</div> 
+
+  <!-- Scripts -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/animation.js"></script>
+  <script src="assets/js/imagesloaded.js"></script>
+  <script src="assets/js/popup.js"></script>
+  <script src="assets/js/custom.js"></script>
+</body>
+</html>
