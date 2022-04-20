9e5c6e85fdc0ce8a0a381c00198da199f7bdf650
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2022-04-20 09:40:17 +0700

    Add files via upload

--- APLIKASI-APS.6.0/index.php
+++ APLIKASI-APS.6.0/index.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,16 +7,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
-<link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>Aplikasi RS. Asura Tugumulyo</title>
+
 <center><button id="goFS">KLIK LAYAR PENUH</button>
 <button type="button" class="get_bg">
  <a href="http://192.168.88.203/dashboard/indexweb.php" >MENU</a>
 </button></center>
 
-<p></p>
+<link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+  <title>DISPLAY TV</title></head>
 <script>
 
   var goFS = document.getElementById("goFS");
@@ -30,178 +31,205 @@
   }, false);
 
 </script>
-<p></p>
+
+
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-<!--
-
-TemplateMo 570 Chain App Dev
-
-https://templatemo.com/tm-570-chain-app-dev
-
--->
-
     <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/templatemo-chain-app-dev.css">
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-space-dynamic.css">
     <link rel="stylesheet" href="assets/css/animated.css">
     <link rel="stylesheet" href="assets/css/owl.css">
+<!--
+    
+TemplateMo 562 Space Dynamic
 
-  <title>Aplikasi RS. Asura Tugumulyo</title></head>
+https://templatemo.com/tm-562-space-dynamic
+
+--></head>
 
 <body>
-<!-- ***** Preloader Start ***** --><!-- ***** Preloader End ***** -->
+
+  <!-- ***** Preloader Start ***** --><!-- ***** Preloader End ***** -->
 
   <!-- ***** Header Area Start ***** --><!-- ***** Header Area End ***** -->
-  
-  <div id="modal" class="popupContainer" style="display:none;">
-    <div class="popupHeader">
-        <span class="header_title">Login</span>
-        <span class="modal_close"><i class="fa fa-times"></i></span>
-    </div>
-
-    <section class="popupBody">
-        <!-- Social Login -->
-        <div class="social_login">
-            <div class="">
-                <a href="#" class="social_box fb">
-                    <span class="icon"><i class="fab fa-facebook"></i></span>
-                    <span class="icon_title">Connect with Facebook</span>
-
-                </a>
-
-                <a href="#" class="social_box google">
-                    <span class="icon"><i class="fab fa-google-plus"></i></span>
-                    <span class="icon_title">Connect with Google</span>
-                </a>
-            </div>
-
-            <div class="centeredText">
-                <span>Or use your Email address</span>
-            </div>
-
-            <div class="action_btns">
-                <div class="one_half"><a href="#" id="login_form" class="btn">Login</a></div>
-                <div class="one_half last"><a href="#" id="register_form" class="btn">Sign up</a></div>
-            </div>
-        </div>
-
-        <!-- Username & Password Login form -->
-        <div class="user_login">
-            <form>
-                <label>Email / Username</label>
-                <input type="text" />
-                <br />
-
-                <label>Password</label>
-                <input type="password" />
-                <br />
-
-                <div class="checkbox">
-                    <input id="remember" type="checkbox" />
-                    <label for="remember">Remember me on this computer</label>
-                </div>
-
-                <div class="action_btns">
-                    <div class="one_half"><a href="#" class="btn back_btn"><i class="fa fa-angle-double-left"></i> Back</a></div>
-                    <div class="one_half last"><a href="#" class="btn btn_red">Login</a></div>
-                </div>
-            </form>
-
-            <a href="#" class="forgot_password">Forgot password?</a>
-        </div>
-
-        <!-- Register Form -->
-        <div class="user_register">
-            <form>
-                <label>Full Name</label>
-                <input type="text" />
-                <br />
-
-                <label>Email Address</label>
-                <input type="email" />
-                <br />
-
-                <label>Password</label>
-                <input type="password" />
-                <br />
-
-                <div class="checkbox">
-                    <input id="send_updates" type="checkbox" />
-                    <label for="send_updates">Send me occasional email updates</label>
-                </div>
-
-                <div class="action_btns">
-                    <div class="one_half"><a href="#" class="btn back_btn"><i class="fa fa-angle-double-left"></i> Back</a></div>
-                    <div class="one_half last"><a href="#" class="btn btn_red">Register</a></div>
-                </div>
-            </form>
-        </div>
-    </section>
-</div>
-
-  
-          </div>
-          </div>
-          </div>
-          </div>
-          </div>
-          </div>
-          </div>
-          </div>
-<div id="pricing" class="pricing-tables">
+  <div id="portfolio" class="our-portfolio section">
     <div class="container">
       <div class="row">
-       
-        <div class="col-lg-4">
-          <div class="pricing-item-pro">
-            <span class="price">APLIKASI</span>
-            <h4>CETAK ANTRIAN</h4>
-            <div class="icon">
-              <img src="https://iconarchive.com/download/i85607/graphicloads/100-flat/print.ico" alt="">            </div>
-            <ul>
-             
-              
-            </ul>
-            <div class="border-button">
-              <a href="CETAK-ANTRIAN/index.php">KLIK</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="pricing-item-pro">
-            <span class="price">APLIKASI</span>
-            <h4>DAFTAR PASIEN</h4>
-            <div class="icon">
-              <img src="https://www.smpn11cilegon.sch.id/upload/picture/13252592daftar-online.png" alt="">            </div>
-            <ul>
-            
-              
-            </ul>
-            <div class="border-button">
-              <a href="DAFTAR-PASIEN/index.php">KLIK</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="pricing-item-pro">
-            <span class="price">APLIKASI</span>
-            <h4>DISPLAY TV </h4>
-            <div class="icon">
-              <img src="https://www.pngall.com/wp-content/uploads/4/LCD-Computer-Monitor-Transparent.png" alt="">            </div>
-            <ul>
-            
-            </ul>
-            <div class="border-button">
-                  <a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/DISPLAY-TV/tv.php">Klik</a>
-        </div>
+        <div class="col-lg-6 offset-lg-3">
+          <div class="section-heading  wow bounceIn" data-wow-duration="1s" data-wow-delay="0.2s">
+            <h2>POLI<em>KLINIK</em></h2>
           </div>
         </div>
       </div>
+      <div class="row">
+        <div class="col-lg-3 col-sm-6">
+          <a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/POLIKLINIK/ANTRIAN-POLI/anjungan/poli">
+            <div class="item wow bounceInUp" data-wow-duration="1s" data-wow-delay="0.3s">
+              <div class="hidden-content">
+                <h4>Antrian Poliklinik</h4>
+               
+              </div>
+              <div class="showed-content">
+                <img src="https://an-nur.ac.id/wp-content/uploads/2018/12/online.png" alt="">              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/POLIKLINIK/JADWALDOKTER">
+            <div class="item wow bounceInUp" data-wow-duration="1s" data-wow-delay="0.4s">
+              <div class="hidden-content">
+                <h4>Jadwal Praktek Dokter</h4>
+                
+              </div>
+              <div class="showed-content">
+                <img src="https://rs-baiturrahim.com/icon/jadwal.png" alt="">
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/POLIKLINIK/sim">
+            <div class="item wow bounceInUp" data-wow-duration="1s" data-wow-delay="0.5s">
+              <div class="hidden-content">
+                <h4>Grafik Poliklinik</h4>
+               
+              </div>
+              <div class="showed-content">
+                <img src="https://icons.iconarchive.com/icons/arrioch/office-dock/256/Color-MS-Excel-icon.png" alt="">
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/POLIKLINIK/KETERSEDIAAN">
+            <div class="item wow bounceInUp" data-wow-duration="1s" data-wow-delay="0.6s">
+              <div class="hidden-content">
+                <h4>Ketersediaan</h4>
+               
+              </div>
+              <div class="showed-content">
+                <img src="https://icon-library.com/images/in-stock-icon/in-stock-icon-0.jpg" alt="">
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
     </div>
-</div> 
+  </div>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  <!-- ***** Header Area Start ***** --><!-- ***** Header Area End ***** -->
+  <div id="portfolio" class="our-portfolio section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 offset-lg-3">
+          <div class="section-heading  wow bounceIn" data-wow-duration="1s" data-wow-delay="0.2s">
+           
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col-sm-6">
+          <a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/POLIKLINIK/CEK-BIAYA">
+            <div class="item wow bounceInUp" data-wow-duration="1s" data-wow-delay="0.3s">
+              <div class="hidden-content">
+                <h4>Cek Biaya Jasa Poliklinik</h4>
+               
+              </div>
+              <div class="showed-content">
+                <img src="http://www.stokjogja.com/wp-content/uploads/2015/12/blurp-rp-icon.png" alt="">              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/POLIKLINIK/cekjadwaloperasi.php">
+            <div class="item wow bounceInUp" data-wow-duration="1s" data-wow-delay="0.4s">
+              <div class="hidden-content">
+                <h4>Cek Jadwal Operasi</h4>
+                
+              </div>
+              <div class="showed-content">
+                <img src="https://cdn-icons-png.flaticon.com/512/194/194919.png" alt="">
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/POLIKLINIK/ANTRIAN-LAB/anjungan/laboratorium">
+            <div class="item wow bounceInUp" data-wow-duration="1s" data-wow-delay="0.5s">
+              <div class="hidden-content">
+                <h4>Antrian Laboratorium</h4>
+               
+              </div>
+              <div class="showed-content">
+                <img src="https://freepngimg.com/thumb/vector/169796-laboratory-vector-set-free-photo.png" alt="">
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <a href="http://192.168.88.203/dashboard/APLIKASI/epasien/">
+            <div class="item wow bounceInUp" data-wow-duration="1s" data-wow-delay="0.6s">
+              <div class="hidden-content">
+                <h4>EPASIEN</h4>
+               
+              </div>
+              <div class="showed-content">
+                <img src="https://cdn-icons-png.flaticon.com/512/504/504079.png" alt="">
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
   <!-- Scripts -->
   <script src="vendor/jquery/jquery.min.js"></script>
@@ -209,7 +237,7 @@ https://templatemo.com/tm-570-chain-app-dev
   <script src="assets/js/owl-carousel.js"></script>
   <script src="assets/js/animation.js"></script>
   <script src="assets/js/imagesloaded.js"></script>
-  <script src="assets/js/popup.js"></script>
-  <script src="assets/js/custom.js"></script>
+  <script src="assets/js/templatemo-custom.js"></script>
+
 </body>
 </html>
