1baffacf21ed3713acda5efe1e86d1aa5b785c43
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2022-06-16 11:58:50 +0700

    Add files via upload

---
+++ DATA/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html><head>
+	<!-- templatemo 418 form pack -->
+    <!-- 
+    Form Pack
+    http://www.templatemo.com/preview/templatemo_418_form_pack 
+    -->
+	<title>Development Program</title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet" type="text/css">
+	<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css">
+	<link href="css/bootstrap-social.css" rel="stylesheet" type="text/css">	
+	<link href="css/templatemo_style.css" rel="stylesheet" type="text/css">	
+	<link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+	 <meta http-equiv="refresh" content="8;url=http://192.168.88.203/dashboard/DATA/index.php">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"><style type="text/css">
+<!--
+body,td,th {
+	color: #FFFFFF;
+}
+body {
+	background-color: #CCCCCC;
+}
+-->
+</style></head>
+
+<body class="templatemo-bg-image-1">
+
+<div class="container">
+		<div class="col-md-12">			
+			<form class="form-horizontal templatemo-login-form-2" role="form" action="#" method="post">
+				<div class="row">
+					<div class="col-md-12">
+						<h1 align="center">Development Program </h1>
+					</div>
+				</div>
+		  <div class="row">
+					<div class="templatemo-one-signin col-md-6">
+				        <div class="form-group">
+				          <div class="col-md-12">		          	
+				            <label for="username" class="control-label">Username</label>
+				            <div class="templatemo-input-icon-container">
+				            	<i class="fa fa-user"></i>
+				            	<input type="text" class="form-control" id="username" placeholder="">
+				            </div>		            		            		            
+				          </div>              
+				        </div>
+				        <div class="form-group">
+				          <div class="col-md-12">
+				            <label for="password" class="control-label">Password</label>
+				            <div class="templatemo-input-icon-container">
+				            	<i class="fa fa-lock"></i>
+				            	<input type="password" class="form-control" id="password" placeholder="">
+				            </div>
+				          </div>
+				        </div>
+				        <div class="form-group">
+				          <div class="col-md-12">
+				            <div class="checkbox">
+			                  <label></label>
+				            </div>
+				          </div>
+				        </div>
+				        <div class="form-group">
+				          <div class="col-md-12"><a href="http://192.168.88.203/dashboard/RIVIEW/index.php">LOGIN</a></div>
+				        </div>
+			          <div class="form-group"></div>
+					</div>
+			        <img src="download.jpeg" width="287" height="235"></div>
+			</form>		      		      
+		</div>
+	</div>
+	<script> 
+     alert("Dilarang Akses Halaman Ini Kecuali Izin Bagian Informasi Teknologi (IT)"); 
+</script> 
+	
+	
+</body>
+</html>
