d23f8d04fb820a9c19e77e37e16c991e15dcd232
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2022-06-22 15:37:01 +0700

    Add files via upload

---
+++ TAMPILAN/phpqrcode/index.php
@@ -0,0 +1,118 @@
+<link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" 
+
+<?php    
+/*
+ * PHP QR Code encoder
+ *
+ * Exemplatory usage
+ *
+ * PHP QR Code is distributed under LGPL 3
+ * Copyright (C) 2010 Dominik Dzienia <title>Aplikasi RS. Asura</title><deltalab at poczta dot fm><style type="text/css">
+<!--
+body {
+	background-color: #999999;
+}
+.style1 {color: #FFFFFF}
+-->
+</style>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+    
+    echo "<h1>BUAT QR CODE </h1>
+    <hr/>";
+    
+    //set it to writable location, a place for temp generated PNG files
+    $PNG_TEMP_DIR = dirname(__FILE__).DIRECTORY_SEPARATOR.'temp'.DIRECTORY_SEPARATOR;
+    
+    //html PNG location prefix
+    $PNG_WEB_DIR = 'temp/';
+
+    include "qrlib.php";    
+    
+    //ofcourse we need rights to create temp dir
+    if (!file_exists($PNG_TEMP_DIR))
+        mkdir($PNG_TEMP_DIR);
+    
+    
+    $filename = $PNG_TEMP_DIR.'test.png';
+    
+    //processing form input
+    //remember to sanitize user input in real-life solution !!!
+    $errorCorrectionLevel = 'L';
+    if (isset($_REQUEST['level']) && in_array($_REQUEST['level'], array('L','M','Q','H')))
+        $errorCorrectionLevel = $_REQUEST['level'];    
+
+    $matrixPointSize = 4;
+    if (isset($_REQUEST['size']))
+        $matrixPointSize = min(max((int)$_REQUEST['size'], 1), 10);
+
+
+    if (isset($_REQUEST['data'])) { 
+    
+        //it's very important!
+        if (trim($_REQUEST['data']) == '')
+            die('data cannot be empty! <a href="?">back</a>');
+            
+        // user data
+        $filename = $PNG_TEMP_DIR.'test'.md5($_REQUEST['data'].'|'.$errorCorrectionLevel.'|'.$matrixPointSize).'.png';
+        QRcode::png($_REQUEST['data'], $filename, $errorCorrectionLevel, $matrixPointSize, 2);    
+        
+    } else {    
+    
+        //default data
+        echo '
+            <hr/>
+            <div align="center">';    
+        QRcode::png('PHP QR Code :)', $filename, $errorCorrectionLevel, $matrixPointSize, 2);    
+        
+    }    
+        
+    //display generated file
+    echo '<img src="'.$PNG_WEB_DIR.basename($filename).'" width="225" height="230" /></div>
+            <hr/>';  
+    
+    //config form
+    echo '<form action="index.php" method="post">
+        <span class="style1">Data:</span>&nbsp;
+        <input name="data" value="'.(isset($_REQUEST['data'])?htmlspecialchars($_REQUEST['data']):'APLIKASI RS. ASURA VERSI 8.0 :)').'" />&nbsp;<span class="style1"> ECC:&nbsp;
+        <select name="level">
+          <option value="L"'.(($errorCorrectionLevel=='L')?' selected':'').'>L - smallest</option>
+          <option value="M"'.(($errorCorrectionLevel=='M')?' selected':'').'>M</option>
+          <option value="Q"'.(($errorCorrectionLevel=='Q')?' selected':'').'>Q</option>
+          <option value="H"'.(($errorCorrectionLevel=='H')?' selected':'').'>H - best</option>
+        </select>
+        &nbsp;
+        Size:&nbsp;
+        <select name="size">
+          ';
+        
+    for($i=1;$i<=10;$i++)
+        echo '
+          <option value="'.$i.'"'.(($matrixPointSize==$i)?' selected':'').'>'.$i.'</option>
+          ';
+        
+    echo '
+        </select>
+        &nbsp;
+        <input type="submit" value="GENERATE">
+        </span>
+    </form>
+    <hr/>';
+        
+    // benchmark
+        
+
+    
