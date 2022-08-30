b6efd2a4f98834396b5b9b4d0348c7d834cfc71e
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2022-08-30 14:59:10 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,331 +1,137 @@
-<link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <style type="text/css">
-  body{
-  padding: 25px;
-  margin : 200px:
-}
-  .style1 {
-	color: #000000;
-	font-family: "Times New Roman", Times, serif;
-}
-  .style2 {font-family: "Times New Roman", Times, serif}
-  .style3 {
-	color: #FF0000;
-	font-size: 14px;
-}
-  .style4 {
-	color: #FF0000;
-	font-family: "Times New Roman", Times, serif;
-	font-size: 14px;
-}
-  .style5 {color: #009900; font-family: "Times New Roman", Times, serif; }
-  </style>
-  </head>
-  <body>
-    <div class="alert alert-success" role="alert">
-      <p>&nbsp;</p>
-      <p>&nbsp;</p>
-      <p>&nbsp;</p>
-      <p class="style1 style2">Selamat Datang Di Aplikasi RS. Asura Versi 8.0 | Desain Hamba Alloh</p>
-      <p class="style1">&nbsp;</p>
-      <p class="style1">Informasi : <span class="style3">Untuk Petugas Pendaftaran Dan Apotik Agar Selalu Koneksi Internet Dulu Sebelum Menggunakan Komputer Dan</span></p>
-      <p class="style4">Untuk Para Petugas Pendaftaran Agar Selalu Memperhatikan Input Data Pasien Agar Tidak Terjadi Kesalahan Data </p>
-      <p class="style1">&nbsp;</p>
-      <p class="style5">UPDATE APLIKASI RS. ASURA VERSI 8.0</p>
-      <p class="style1">&nbsp;</p>
-      <p class="style1">* SUDAH ADA DAFTAR HARGA OBAT DI MENU DEVELOPMENT PROGRAM</p>
-      <p class="style1">* SUDAH ADA MENU DEVELOPMENT PROGRAM</p>
-      <p class="style1">* SUDAH ADA APLIKASI APOTIK</p>
-      <p class="style1">* SUDAH ADA APLIKASI KEUANGAN</p>
-      <p class="style1">* SUDAH ADA MENU APOTIK</p>
-      <p class="style1">* SUDAH ADA MENU CEK TAGIHAN PBB &amp; TELKOM (WIFI) </p>
-      <p class="style1">* SUDAH ADA MENU MANAGEMENT</p>
-      <p class="style1">* SUDAH ADA MENU CETAK</p>
-      <p class="style1">&nbsp;</p>
-      <p class="style1">CATATAN : <a href="http://192.168.88.203/dashboard/NOTIFIKASI/INFO.jpg" target="_blank">CARA MENGHIDUPKAN CPU SERVER SIMRS (KLIK) </a></p>
-      <p class="style1">&nbsp;</p>
-      <p class="style1"><a href="http://192.168.88.203/dashboard/kritikdansaran.html">BERIKAN KRITIK DAN SARAN (KLIK) </a></p>
-      <p class="style1">&nbsp;</p>
-      <p class="style5">SEMANGAT SELALU DAN PROFESIONAL, SEMOGA BERKAH DAN BERMANFAAT</p>
-    </div>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-  </body>
-  <script type="text/javascript">
-  window.setTimeout(function() {
-    $(".alert").fadeTo(500, 0).slideUp(500, function(){
-        $(this).remove(); 
-    });
-}, 8000);
-</script>
-
-
-
-
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-  <head>
-  
-  
-  
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
-<link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
-    <title>Aplikasi RS. Asura</title>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <meta name="description" content="">
+        <meta name="author" content="TemplateMo">
+
+        <title>Aplikasi RS. Asura</title>
+
+        <!-- CSS FILES -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+
+        <link href="http://192.168.88.203/dashboard/SISTEM8.0/css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="http://192.168.88.203/dashboard/SISTEM8.0/css/bootstrap-icons.css" rel="stylesheet">
+
+        <link href="http://192.168.88.203/dashboard/SISTEM8.0/css/magnific-popup.css" rel="stylesheet">
+
+        <link href="http://192.168.88.203/dashboard/SISTEM8.0/css/templatemo-first-portfolio-style.css" rel="stylesheet">
+     <link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+	 
+	  <meta http-equiv="refresh" content="8;url=http://192.168.88.203/dashboard/HALAMAN/">    
 <!--
 
-Breezed Template
+TemplateMo 578 First Portfolio
 
-https://templatemo.com/tm-543-breezed
+https://templatemo.com/tm-578-first-portfolio
 
 -->
-    <!-- Additional CSS Files -->
-    <link rel="stylesheet" type="text/css" href="MASTER8.0/assets/css/bootstrap.min.css">
-
-    <link rel="stylesheet" type="text/css" href="MASTER8.0/assets/css/font-awesome.css">
-
-    <link rel="stylesheet" href="MASTER8.0/assets/css/templatemo-breezed.css">
-
-    <link rel="stylesheet" href="MASTER8.0/assets/css/owl-carousel.css">
-
-    <link rel="stylesheet" href="MASTER8.0/assets/css/lightbox.css">
-
     </head>
     
     <body>
-    
-    <!-- ***** Preloader Start ***** -->
-    <div id="preloader">
-        <div class="jumper">
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-    </div>  
-    <!-- ***** Preloader End ***** -->
-    
-    
-    <!-- ***** Header Area Start ***** -->
-    <header class="header-area header-sticky">
-        <div class="container">
-		
-		
-		
-		
-            <div class="row">
-                <div class="col-12">
-                  <nav class="main-nav">
-                        <!-- ***** Logo Start ***** -->
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-						
-                        <a href="index.php" class="logo">
-                          RS. Asura                        </a>
-						  
-                        <!-- ***** Logo End ***** -->
-                        <!-- ***** Menu Start ***** -->
-<ul class="nav">
-                            <li class="scroll-to-section"><a href="http://192.168.88.203/dashboard/indexutama.php" class="active">Menu Utama</a></li>
-                            <li class="scroll-to-section"><a href="http://192.168.88.203/dashboard/indexmanagement.php">Menu Management</a></li>
-                            <li class="scroll-to-section"><a href="http://192.168.88.203/dashboard/indexweb.php">Menu Halaman</a></li>
-                            <li class="scroll-to-section"><a href="http://192.168.88.203/dashboard/indexmenu.php">Menu Beranda</a></li>
-							                            <div class="search-icon">
-                                <a href="#search"><i class="fa fa-search"></i></a>                            </div>
-                        </ul>        
-                        <a class='menu-trigger'>
-                            <span>Menu</span>                      </a>
-                      <!-- ***** Menu End ***** -->
-                  </nav>
-                </div>
-            </div>
-        </div>
-    </header>
-    <!-- ***** Header Area End ***** -->
-    
-    <!-- ***** Search Area ***** -->
-    <div id="search">
-          <button type="button" class="close">×</button>
-       <form id="contact" action="#" method="get">
-            <fieldset>
-                <input type="search" name="q" placeholder=" DESAIN HAMBA ALLOH" aria-label="Search through site content">
-            </fieldset>
-            <fieldset>
-                <center><button type="submit" class="main-button">Versi 8.0</button>
-                </center>
-            </fieldset>
-      </form>
-		
-		
-    </div>
 
-    <!-- ***** Main Banner Area Start ***** -->
-	
-	
-	
-	
-	
-	 
-    
-    
-
-	
-	
-	
-    <div class="main-banner header-text" id="top">
-        <div class="Modern-Slider">
-          <!-- Item -->
-          <div class="item">
-            <div class="img-fill">
-                <img src="MASTER8.0/assets/images/1.png" alt="">
-                <div class="text-content">
-                  <h3>Menu Utama</h3>
-                  <h5>Fitur Cek Administrasi Dan Tagihan</h5>
-                  <a href="http://192.168.88.203/dashboard/bantuan.html" class="main-stroked-button">Bantuan</a>
-                  <a href="http://192.168.88.203/dashboard/indexutama.php" class="main-filled-button">Klik</a>                </div>
-            </div>
-          </div>
-          <!-- // Item -->
-          <!-- Item -->
-          <div class="item">
-            <div class="img-fill">
-                <img src="MASTER8.0/assets/images/2.png" alt="">
-                <div class="text-content">
-                  <h3>Menu Management</h3>
-                  <h5>Fitur Management Dan Kepegawaian</h5>
-                 <a href="http://192.168.88.203/dashboard/bantuan.html" class="main-stroked-button">Bantuan</a>
-                  <a href="http://192.168.88.203/dashboard/indexmanagement.php" class="main-filled-button">Klik</a>                </div>
-            </div>
-          </div>
-          <!-- // Item -->
-          <!-- Item -->
-          <div class="item">
-            <div class="img-fill">
-                <img src="MASTER8.0/assets/images/3.jpg" alt="">
-                <div class="text-content">
-                  <h3>Menu Halaman</h3>
-                  <h5>Fitur Display Tampilan Dan Antrian Pendaftaran</h5>
-                  <a href="http://192.168.88.203/dashboard/bantuan.html" class="main-stroked-button">Bantuan</a>
-                  <a href="http://192.168.88.203/dashboard/indexweb.php" class="main-filled-button">Klik</a>                </div>
-            </div>
-          </div>
-          <!-- // Item -->
-		  !-- Item --><div class="item">
-		    <div class="img-fill">
-                <img src="MASTER8.0/assets/images/4.jpeg" alt="">
-                <div class="text-content">
-                  <h3>Menu Beranda</h3>
-                  <h5>Fitur Petugas Dan Informasi</h5>
-                  <<a href="http://192.168.88.203/dashboard/bantuan.html" class="main-stroked-button">Bantuan</a>
-                  <a href="http://192.168.88.203/dashboard/indexmenu.php" class="main-filled-button">Klik</a>                </div>
-            </div>
-          </div>
-          <!-- // Item -->
-        </div>
-
-    
-    <!-- ***** Main Banner Area End ***** -->
-
-
-
-  
-	
-	 <iframe src="http://192.168.88.203/dashboard/MASTER8.0/sim/index.php" name="frame" width="100%" height="600px" allowtransparency="true" noresize="noresize"></iframe>
-	
-	
-	
-	
-  
+        
+        <main>
 
 
 
 
-    
 
-    <footer>
-        <div class="container">
-          <div class="row">
-                <div class="col-lg-6 col-xs-12">
-                    <div class="left-text-content">
-                        <p>Copyright &copy; Aplikasi RS. Asura Versi 8.0 <a rel="nofollow noopener" href=""></a></p>
-                  </div>
-                </div>
-                <div class="col-lg-6 col-xs-12">
-                  <div class="col-lg-6 col-xs-12">
-                    <div class="left-text-content">
-                      <p align="right"><a href="http://192.168.88.203/dashboard/DATA/index.php" target=""> Development Program </a></p>
+            <section class="clients section-padding">
+                <div class="container">
+                    <div class="row align-items-center">
+
+                        <div class="col-lg-12 col-12">
+                            <h3 class="text-center mb-5">RS. ASURA </h3>
+                        </div>
+
+                        <div class="col-lg-2 col-4 ms-auto clients-item-height">
+                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/KEMENKES.jpg" class="clients-image img-fluid" alt="">
+                        </div>
+
+                        <div class="col-lg-2 col-4 clients-item-height">
+                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/ADMEDIKA.png" class="clients-image img-fluid" alt="">
+                        </div>
+
+                        <div class="col-lg-2 col-4 clients-item-height">
+                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/BPJS.jpg" class="clients-image img-fluid" alt="">
+                        </div>
+
+                        <div class="col-lg-2 col-4 clients-item-height">
+                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/BSI.jpg" class="clients-image img-fluid" alt="">
+                        </div>
+
+                        <div class="col-lg-2 col-4 me-auto clients-item-height">
+                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/INHEALTH.jpg" class="clients-image img-fluid" alt="">
+                        </div>
+						
+                        <div class="col-lg-2 col-4 me-auto clients-item-height">
+                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/SIMRS.jpeg" class="clients-image img-fluid" alt="">
+                        </div>
+						
+						 <div class="col-lg-2 col-4 me-auto clients-item-height">
+                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/BIO.jpg" class="clients-image img-fluid" alt="">
+                        </div>
+						
+						
+						 <div class="col-lg-2 col-4 me-auto clients-item-height">
+                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/BRI.jpg" class="clients-image img-fluid" alt="">
+                        </div>
+						
+						 <div class="col-lg-2 col-4 me-auto clients-item-height">
+                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/DEX.png" class="clients-image img-fluid" alt="">
+                        </div>
+						
+						<div class="col-lg-2 col-4 me-auto clients-item-height">
+                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/JAT.jpeg" class="clients-image img-fluid" alt="">
+                        </div>
+						
+						<div class="col-lg-2 col-4 me-auto clients-item-height">
+                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/KIM.jpg" class="clients-image img-fluid" alt="">
+                        </div>
+						
+						<div class="col-lg-2 col-4 me-auto clients-item-height">
+                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/SAN.png" class="clients-image img-fluid" alt="">
+                        </div>
+
                     </div>
-                  </div>
-                    <div class="right-text-content">                    </div>
+                </div>
+            </section>
+
+
+         
+        </main>
+
+    <footer class="site-footer">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-12 col-12">
+                        <div class="copyright-text-wrap">
+                            <p align="center" class="mb-0">
+                                <span class="copyright-text">Copyright © Aplikasi RS. Asura Versi 8.0 <a href=""></a></span>                            </p>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-          </div>
-        </div>
-    </footer>
-    
+        </footer>
 
+        <!-- JAVASCRIPT FILES -->
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/jquery.min.js"></script>
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/bootstrap.min.js"></script>
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/jquery.sticky.js"></script>
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/click-scroll.js"></script>
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/jquery.magnific-popup.min.js"></script>
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/magnific-popup-options.js"></script>
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/custom.js"></script>
 
-
-
-
-
-
-
-
-    <!-- jQuery -->
-    <script src="MASTER8.0/assets/js/jquery-2.1.0.min.js"></script>
-
-    <!-- Bootstrap -->
-    <script src="MASTER8.0/assets/js/popper.js"></script>
-    <script src="MASTER8.0/assets/js/bootstrap.min.js"></script>
-
-    <!-- Plugins -->
-    <script src="MASTER8.0/assets/js/owl-carousel.js"></script>
-    <script src="MASTER8.0/assets/js/scrollreveal.min.js"></script>
-    <script src="MASTER8.0/assets/js/waypoints.min.js"></script>
-    <script src="MASTER8.0/assets/js/jquery.counterup.min.js"></script>
-    <script src="MASTER8.0/assets/js/imgfix.min.js"></script> 
-    <script src="MASTER8.0/assets/js/slick.js"></script> 
-    <script src="MASTER8.0/assets/js/lightbox.js"></script> 
-    <script src="MASTER8.0/assets/js/isotope.js"></script> 
-    
-    <!-- Global Init -->
-    <script src="MASTER8.0/assets/js/custom.js"></script>
-
-<a href="http://192.168.88.203/dashboard/DOWNLOAD-APLIKASI">
-<img src="http://192.168.88.203/dashboard/DOWNLOAD-APLIKASI/DOWNLOAD.png" class="wabutton" alt="Download Aplikasi" />
-</a>
-<style>
-.wabutton{
-width:80px;
-height:80px;
-position:fixed;
-bottom:20px;
-right:20px;
-z-index:100;
-}
-</style>
-
-
-
-
-
-
-
-  </body>
+    </body>
 </html>
