c320baa47da3e400a0edc51e35c9ab2f109182ec
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2022-08-30 15:32:56 +0700

    Add files via upload

---
+++ APLIKASI80/index.php
@@ -0,0 +1,184 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="">
+        <meta name="author" content="TemplateMo">
+
+        <title>Aplikasi RS. Asura</title>
+
+        <!-- CSS FILES -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+
+        <link href="http://192.168.88.203/dashboard/SISTEM8.0/css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="http://192.168.88.203/dashboard/SISTEM8.0/css/bootstrap-icons.css" rel="stylesheet">
+
+        <link href="http://192.168.88.203/dashboard/SISTEM8.0/css/magnific-popup.css" rel="stylesheet">
+
+        <link href="http://192.168.88.203/dashboard/SISTEM8.0/css/templatemo-first-portfolio-style.css" rel="stylesheet">
+          <link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+<!--
+
+TemplateMo 578 First Portfolio
+
+https://templatemo.com/tm-578-first-portfolio
+
+-->
+    </head>
+    
+    <body>
+
+    
+
+            
+
+        
+
+          <section class="projects section-padding" id="section_4">
+                <div class="container">
+                    <div class="row">
+                      <div class="col-lg-4 col-md-6 col-12">
+                        <div class="projects-thumb">
+                                <div class="projects-info">
+                                    <small class="projects-tag">MENU MASTER</small>
+
+                                    <h3 class="projects-title">Informasi Menu </h3>
+                                </div>
+
+                           <a href="http://192.168.88.203/dashboard/indexsistem.php"  target="_blank" >
+                                <img src="http://192.168.88.203/dashboard/SISTEM8.0/MASTER.jpg" alt="" width="860" height="901" class="projects-image img-fluid">                          </a>                          </div>
+                      </div>
+
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="projects-thumb">
+                                <div class="projects-info">
+                                    <small class="projects-tag">MENU UTAMA</small>
+
+                                    <h3 class="projects-title">Cek Tagihan & Administrasi</h3>
+                                </div>
+
+                                <a href="http://192.168.88.203/dashboard/indexutama.php"  target="_blank" >
+                                    <img src="http://192.168.88.203/dashboard/SISTEM8.0/TAGIHAN.jpg" class="projects-image img-fluid" alt="">
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="projects-thumb">
+                                <div class="projects-info">
+                                    <small class="projects-tag">MENU MANAGEMENT</small>
+
+                                    <h3 class="projects-title">Karyawan & Management</h3>
+                                </div>
+
+                                <a href="http://192.168.88.203/dashboard/indexmanagement.php"  target="_blank" >
+                                    <img src="http://192.168.88.203/dashboard/SISTEM8.0/KARYAWAN.jpg" class="projects-image img-fluid" alt="">
+                                </a>
+                            </div>
+                        </div>
+
+                  </div>
+        </main>
+		
+		
+		
+		
+		
+		
+          <section class="projects section-padding" id="section_4">
+                <div class="container">
+                    <div class="row">
+                      <div class="col-lg-4 col-md-6 col-12">
+                        <div class="projects-thumb">
+                                <div class="projects-info">
+                                    <small class="projects-tag">Menu Halaman </small>
+
+                                    <h3 class="projects-title">Poliklinik & Pelayanan </h3>
+                                </div>
+
+                           <a href="http://192.168.88.203/dashboard/indexweb.php"  target="_blank" >
+                                <img src="http://192.168.88.203/dashboard/SISTEM8.0/POLIKLINIK.png" alt="" width="860" height="901" class="projects-image img-fluid">                          </a>                          </div>
+                      </div>
+
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="projects-thumb">
+                                <div class="projects-info">
+                                    <small class="projects-tag">Menu Beranda</small>
+
+                                    <h3 class="projects-title">Informasi & Aplikasi</h3>
+                                </div>
+
+                                <a href="http://192.168.88.203/dashboard/indexmenu.php"  target="_blank" >
+                                    <img src="http://192.168.88.203/dashboard/SISTEM8.0/APLIKASI.png" class="projects-image img-fluid" alt="">
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="projects-thumb">
+                                <div class="projects-info">
+                                    <small class="projects-tag">Menu Cetak</small>
+
+                                    <h3 class="projects-title">Informasi Berkas & FIle</h3>
+                                </div>
+
+                                <a href="http://192.168.88.203/dashboard/pendaftaran.html"  target="_blank" >
+                                    <img src="http://192.168.88.203/dashboard/SISTEM8.0/CETAK.jpg" class="projects-image img-fluid" alt="">
+                                </a>
+                            </div>
+                        </div>
+
+                  </div>
+           
+        </main>
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+        <footer class="">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-12 col-12">
+                        <div class="copyright-text-wrap">
+                            <p align="center" class="mb-0">
+                                <span class="copyright-text">Copyright © Aplikasi RS. Asura Versi 8.0 <a href=""></a></span>
+                               
+                                <a rel="sponsored" href="" target="_blank"></a>                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </footer>
+
+        <!-- JAVASCRIPT FILES -->
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/jquery.min.js"></script>
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/bootstrap.min.js"></script>
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/jquery.sticky.js"></script>
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/click-scroll.js"></script>
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/jquery.magnific-popup.min.js"></script>
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/magnific-popup-options.js"></script>
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/custom.js"></script>
+
+    </body>
+</html>
