214dac710730bf569d8996b9eaef443ea6ea906b
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2022-11-30 12:40:03 +0700

    Add files via upload

---
+++ MASTEROK/akses.php
@@ -0,0 +1,188 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Aplikasi RS. Asura</title>
+
+        <!-- CSS FILES -->        
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="css/bootstrap-icons.css" rel="stylesheet">
+
+        <link href="css/templatemo-kind-heart-charity.css" rel="stylesheet">
+		<link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+<!--
+
+TemplateMo 581 Kind Heart Charity
+
+https://templatemo.com/tm-581-kind-heart-charity
+
+-->
+
+    </head>
+    
+    <body id="section_1">
+
+    <header class="site-header"></header>
+        <main>
+
+            <section class="hero-section hero-section-full-height">
+              <div class="container-fluid"></div>
+            </section>
+
+
+            <section class="section-padding">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-lg-10 col-12 text-center mx-auto">
+                            <h2 class="mb-5">&nbsp;</h2>
+                        </div>
+
+                        <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0">
+                            <div class="featured-block d-flex justify-content-center align-items-center">
+                                <a href="http://192.168.88.203/dashboard/APLIKASI80/"  target="_blank"class="d-block">
+                                    <img src="http://192.168.88.203/dashboard/SISTEM8.0/MENU.png" class="featured-block-image img-fluid" alt="">
+
+                                    <p class="featured-block-text">MENU RS. ASURA</p>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0 mb-md-4">
+                            <div class="featured-block d-flex justify-content-center align-items-center">
+                                <a href="http://192.168.88.1/webfig/#Wireless.Interfaces.5" target="_blank" class="d-block">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/4425/4425276.png" class="featured-block-image img-fluid" alt="">
+
+                                    <p class="featured-block-text">KONEKSI INTERNET</p>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0 mb-md-4">
+                            <div class="featured-block d-flex justify-content-center align-items-center">
+                                <a href="http://192.168.88.203/dashboard/MENU-ADMIN/index.php" target="_blank" class="d-block">
+                                    <img src="http://192.168.88.203/dashboard/SISTEM8.0/administrasi.png" class="featured-block-image img-fluid" alt="">
+
+                                    <p class="featured-block-text">ADMINISTRASI</p>
+                                </a>
+                            </div>
+                        </div>
+
+
+
+
+<div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0 mb-md-4">
+                            <div class="featured-block d-flex justify-content-center align-items-center">
+                                <a href="http://192.168.88.203/dashboard/MENU-APOTIK/index.php" target="_blank" class="d-block">
+                                    <img src="http://192.168.88.203/dashboard/KEUANGAN/OBAT.png" class="featured-block-image img-fluid" alt="">
+
+                                    <p class="featured-block-text">INSTALASI FARMASI</p>
+                                </a>
+                            </div>
+                      </div>
+
+<div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0 mb-md-4">
+                            <div class="featured-block d-flex justify-content-center align-items-center">
+                                <a href="http://192.168.88.203/dashboard/MENU-MANAGEMENT/index.php" target="_blank" class="d-block">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS847kWElWG673CkQIuYJVaYke5APbXW7jSXF9ihVrQkxonWXXnrHs8Uc3wu-FDnTjMU9Y&usqp=CAU" alt="">
+
+                                    <p class="featured-block-text">INFORMASI TEKNOLOGI</p>
+                                </a>
+                            </div>
+                      </div>
+
+
+
+
+
+
+
+
+
+
+
+<div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0 mb-md-4">
+                            <div class="featured-block d-flex justify-content-center align-items-center">
+                                <a href="http://192.168.88.203/dashboard/APLIKASI/KHANZALITE-PAKESUARA/" target="_blank" class="d-block">
+                                    <img src="https://cdn.pixabay.com/photo/2017/05/15/21/51/blood-icon-2316227_960_720.png" class="featured-block-image img-fluid" alt="">
+
+                                    <p class="featured-block-text">PARAMEDIS</p>
+                                </a>
+                            </div>
+                      </div>
+
+
+
+
+<div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0 mb-md-4">
+                            <div class="featured-block d-flex justify-content-center align-items-center">
+                                <a href="http://192.168.88.203/dashboard/APLIKASI/dokter/login.php" target="_blank" class="d-block">
+                                    <img src="http://192.168.88.203/dashboard/SISTEM8.0/dokter.png" class="featured-block-image img-fluid" alt="">
+
+                                    <p class="featured-block-text">DOKTER</p>
+                                </a>
+                            </div>
+                      </div>
+
+
+
+
+
+
+
+
+
+
+                        <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0">
+                            <div class="featured-block d-flex justify-content-center align-items-center">
+                                <a href="http://192.168.88.203/dashboard/KEUANGAN/index.php"  target="_blank"class="d-block">
+                                    <img src="http://192.168.88.203/dashboard/SISTEM8.0/TAGIHAN.jpg" class="featured-block-image img-fluid" alt="">
+
+                                    <p class="featured-block-text">KEUANGAN</p>
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+          
+
+        
+
+    <footer class="">
+      <div class="site-footer-bottom">
+                <div class="container">
+                    <div class="row">
+
+                        
+                            <center><p class="copyright-text mb-0">Copyright © Aplikasi RS. Asura Versi 8.0  </center>
+                        	
+                  </div>
+                        
+                        
+                            </ul>
+        </div>
+                        
+      </div>
+                </div>
+            </div>
+    </footer>
+
+        <!-- JAVASCRIPT FILES -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.sticky.js"></script>
+        <script src="js/click-scroll.js"></script>
+        <script src="js/counter.js"></script>
+        <script src="js/custom.js"></script>
+
+    </body>
+</html>
---
+++ MASTEROK/index.php
@@ -0,0 +1,322 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Aplikasi RS. Asura</title>
+
+        <!-- CSS FILES -->        
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="css/bootstrap-icons.css" rel="stylesheet">
+
+        <link href="css/templatemo-kind-heart-charity.css" rel="stylesheet">
+		<link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+<!--
+
+TemplateMo 581 Kind Heart Charity
+
+https://templatemo.com/tm-581-kind-heart-charity
+
+-->
+
+    </head>
+    
+    <body id="section_1">
+
+        <header class="site-header">
+            <div class="container">
+                <div class="row">
+                    
+                    <div class="col-lg-8 col-12 d-flex flex-wrap">
+                        <p class="d-flex me-4 mb-0">
+                            <i class="bi-geo-alt me-2"></i> 
+                            <a href="https://goo.gl/maps/sxBhEVx8rjm1CK756"target="blank">Jl. Lintas Timur Desa Tugumulyo Kec. Lempuing Kab. OKI
+                        </p>
+
+                        <p class="d-flex mb-0">
+                            <i class="bi-envelope me-2"></i>
+
+                            <a href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=asura.11022022@gmail.com"target="blank">
+                                asura.11022022@gmail.com
+                            </a>
+                        </p>
+                    </div>
+
+                    <div class="col-lg-3 col-12 ms-auto d-lg-block d-none">
+                        <ul class="social-icon">
+                            
+
+                            <li class="social-icon-item">
+                                <a href="https://www.facebook.com/klinik.asura"target="blank" class="social-icon-link bi-facebook"></a>
+                            </li>
+
+                            <li class="social-icon-item">
+                                <a href="https://www.instagram.com/klinik_asura/" target="blank"class="social-icon-link bi-instagram"></a>
+                            </li>
+
+                            <li class="social-icon-item">
+                                <a href="https://www.youtube.com/channel/UCEZ8E_FxPFwdHzcL65pucug" class="social-icon-link bi-youtube"target="blank"></a>
+                            </li>
+
+                            <li class="social-icon-item">
+                                <a href="https://api.whatsapp.com/send?phone=6281271138811" target="blank"class="social-icon-link bi-whatsapp"></a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </header>
+
+        <nav class="navbar navbar-expand-lg bg-light shadow-lg">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">
+                    <img src="http://192.168.88.203/dashboard/download.jpeg" class="logo img-fluid" alt="Kind Heart Charity">
+                    <span>
+                        RS. Asura
+                        <small>APLIKASI VERSI 8.0 | DESAIN HAMBA ALLOH</small>                    </span>                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                  <ul class="navbar-nav ms-auto"><li class="nav-item"></li>
+
+                      <li class="nav-item">
+                          <a class="nav-link click-scroll" href="#section_3"></a>                      </li>
+
+                      <li class="nav-item"></li>
+
+                      <li class="nav-item dropdown">
+                         
+
+                          <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
+                              
+
+                             
+                          </ul>
+                      </li>
+
+                      <li class="nav-item">
+                          <a class="nav-link click-scroll" href="#section_6">Selamat Datang di Aplikasi RS. ASURA | SIMRS KHANZA KONEKSI</a>                      </li>
+
+                      <li class="nav-item ms-3">
+                          <a class="nav-link custom-btn custom-border-btn btn" href="http://192.168.88.203/dashboard/DOWNLOAD-APLIKASI/" target="blank">DOWNLOAD</a>                      </li>
+                    </ul>
+              </div>
+          </div>
+        </nav>
+
+        <main>
+
+            <section class="hero-section hero-section-full-height">
+                <div class="container-fluid">
+                    <div class="row">
+
+                        <div class="col-lg-12 col-12 p-0">
+                            <div id="hero-slide" class="carousel carousel-fade slide" data-bs-ride="carousel">
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img src="https://static.republika.co.id/uploads/images/inpicture_slide/prospek-karier-lulusan-teknologi-komputer-selain-menjadi-start-up_220107104356-793.jpg" class="carousel-image img-fluid" alt="...">
+                                        
+                                        <div class="carousel-caption d-flex flex-column justify-content-end">
+                                            <h1>RS. ASURA</h1>
+                                            
+                                            <p>EMERGENCY KEBIDANAN 24 JAM</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="carousel-item">
+                                        <img src="https://miro.medium.com/max/1400/1*jsyM9COYU7WemlKvqV2GrQ.jpeg" class="carousel-image img-fluid" alt="...">
+                                        
+                                        <div class="carousel-caption d-flex flex-column justify-content-end">
+                                            <h1>RS. ASURA</h1>
+                                            
+                                            <p>MELAYANI IGD 24 JAM</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="carousel-item">
+                                        <img src="https://cdn.pixabay.com/photo/2020/09/29/03/01/technology-5611570_640.jpg" class="carousel-image img-fluid" alt="...">
+                                        
+                                        <div class="carousel-caption d-flex flex-column justify-content-end">
+                                            <h1>RS. ASURA</h1>
+                                            
+                                            <p>MELAYANI DENGAN CEPAT, PROFESIONAL DAN AMAN</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <button class="carousel-control-prev" type="button" data-bs-target="#hero-slide" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+
+                                <button class="carousel-control-next" type="button" data-bs-target="#hero-slide" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+
+            <section class="section-padding">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-lg-10 col-12 text-center mx-auto">
+                            <h2 class="mb-5">&nbsp;</h2>
+                        </div>
+
+                        <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0">
+                            <div class="featured-block d-flex justify-content-center align-items-center">
+                                <a href="http://192.168.88.203/dashboard/APLIKASI80/"  target="_blank"class="d-block">
+                                    <img src="http://192.168.88.203/dashboard/SISTEM8.0/MENU.png" class="featured-block-image img-fluid" alt="">
+
+                                    <p class="featured-block-text">MENU RS. ASURA</p>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0 mb-md-4">
+                            <div class="featured-block d-flex justify-content-center align-items-center">
+                                <a href="http://192.168.88.1/webfig/#Wireless.Interfaces.5" target="_blank" class="d-block">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/4425/4425276.png" class="featured-block-image img-fluid" alt="">
+
+                                    <p class="featured-block-text">KONEKSI INTERNET</p>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0 mb-md-4">
+                            <div class="featured-block d-flex justify-content-center align-items-center">
+                                <a href="http://192.168.88.203/dashboard/MENU-ADMIN/index.php" target="_blank" class="d-block">
+                                    <img src="http://192.168.88.203/dashboard/SISTEM8.0/administrasi.png" class="featured-block-image img-fluid" alt="">
+
+                                    <p class="featured-block-text">ADMINISTRASI</p>
+                                </a>
+                            </div>
+                        </div>
+
+
+
+
+<div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0 mb-md-4">
+                            <div class="featured-block d-flex justify-content-center align-items-center">
+                                <a href="http://192.168.88.203/dashboard/MENU-APOTIK/index.php" target="_blank" class="d-block">
+                                    <img src="http://192.168.88.203/dashboard/KEUANGAN/OBAT.png" class="featured-block-image img-fluid" alt="">
+
+                                    <p class="featured-block-text">INSTALASI FARMASI</p>
+                                </a>
+                            </div>
+                        </div>
+
+<div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0 mb-md-4">
+                            <div class="featured-block d-flex justify-content-center align-items-center">
+                                <a href="http://192.168.88.203/dashboard/MENU-MANAGEMENT/index.php" target="_blank" class="d-block">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS847kWElWG673CkQIuYJVaYke5APbXW7jSXF9ihVrQkxonWXXnrHs8Uc3wu-FDnTjMU9Y&usqp=CAU" alt="">
+
+                                    <p class="featured-block-text">INFORMASI TEKNOLOGI</p>
+                                </a>
+                            </div>
+                        </div>
+
+
+
+
+
+
+
+
+
+
+
+<div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0 mb-md-4">
+                            <div class="featured-block d-flex justify-content-center align-items-center">
+                                <a href="http://192.168.88.203/dashboard/APLIKASI/KHANZALITE-PAKESUARA/" target="_blank" class="d-block">
+                                    <img src="https://cdn.pixabay.com/photo/2017/05/15/21/51/blood-icon-2316227_960_720.png" class="featured-block-image img-fluid" alt="">
+
+                                    <p class="featured-block-text">PARAMEDIS</p>
+                                </a>
+                            </div>
+                        </div>
+
+
+
+
+<div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0 mb-md-4">
+                            <div class="featured-block d-flex justify-content-center align-items-center">
+                                <a href="http://192.168.88.203/dashboard/APLIKASI/dokter/login.php" target="_blank" class="d-block">
+                                    <img src="http://192.168.88.203/dashboard/SISTEM8.0/dokter.png" class="featured-block-image img-fluid" alt="">
+
+                                    <p class="featured-block-text">DOKTER</p>
+                                </a>
+                            </div>
+                        </div>
+
+
+
+
+
+
+
+
+
+
+                        <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0">
+                            <div class="featured-block d-flex justify-content-center align-items-center">
+                                <a href="http://192.168.88.203/dashboard/KEUANGAN/index.php"  target="_blank"class="d-block">
+                                    <img src="http://192.168.88.203/dashboard/SISTEM8.0/TAGIHAN.jpg" class="featured-block-image img-fluid" alt="">
+
+                                    <p class="featured-block-text">KEUANGAN</p>
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+          
+
+        
+
+    <footer class="">
+      <div class="site-footer-bottom">
+                <div class="container">
+                    <div class="row">
+
+                        
+                            <center><p class="copyright-text mb-0">Copyright © Aplikasi RS. Asura Versi 8.0  </center>
+                        	
+                        </div>
+                        
+                        
+                            </ul>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        <!-- JAVASCRIPT FILES -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.sticky.js"></script>
+        <script src="js/click-scroll.js"></script>
+        <script src="js/counter.js"></script>
+        <script src="js/custom.js"></script>
+
+    </body>
+</html>
