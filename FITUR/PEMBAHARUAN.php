abe06769bf794b2906014019171c5a55cbad4b5d
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2024-08-04 14:49:22 +0700

    Add files via upload

---
+++ FITUR/PEMBAHARUAN.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Aplikasi RS. Asura</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+ <link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+ <style type="text/css">
+<!--
+.style1 {
+	font-size: 24px;
+	font-weight: bold;
+}
+.style2 {color: #009900}
+.style3 {color: #FF0000}
+-->
+ </style>
+
+  
+
+ <p class="style1">UPDATE : 31 MARET 2024 11:44 WIB</p>
+<p class="style2">APLIKASI RS. ASURA VERSI 8.0   </p>
+<p class="style2">Koneksi 192.168.88.203 SERVER UTAMA </p>
+<p class="style3">MAINTENANCE IT :   </p>
+<p>- UPDATE ICON ANTARMUKA WELCOME   </p>
+ <p>- UPDATE PERSETUJUAN MASUK APLIKASI</p>
+ <p> - TAMBAH FITUR   </p>
+ <p>- UPDATE APLIKASI</p>
+ <p> - UPDATE KEAMANAN SISTEM APLIKASI   </p>
+ <p>- UPDATE SISTEM CODING APLIKASI   </p>
+ <p>- UPDATE KERAPIHAN MENU APLIKASI   </p>
+ <p>- UPDATE KONEKSI SIMRS DAN APLIKASI </p>
---
+++ FITUR/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Aplikasi RS. Asura</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+ <link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+  
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+<meta http-equiv="refresh" content="20;url=http://192.168.88.203/dashboard/TAMPILAN-APLIKASI/TEMPLATE-APLIKASI">  
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+
+<audio autoplay>
+      <source src="http://192.168.88.203/dashboard/AUDIO/TAMPILAN.mp3" type="audio/mpeg">
+    </audio>
+
+
+  <!-- =======================================================
+  * Template Name: Bootslander
+  * Template URL: https://bootstrapmade.com/bootslander-free-bootstrap-landing-page-template/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div class="logo">
+        <h1><a href="index.php"><span>RS. ASURA</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="http://192.168.88.203/dashboard/FITUR/">Beranda</a></li>
+		  <li><a class="nav-link scrollto" href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/DAFTAR-PASIEN/index-daftarpasien.php"target="_blank">Daftar Pasien</a></li>
+         
+           <li><a class="nav-link scrollto" href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/CETAK-ANTRIAN/index-cetakantrian.php"target="_blank">Antrian</a></li>
+         
+    
+          <li><a class="nav-link scrollto" href="http://192.168.88.203/dashboard/FITUR/ANTRIAN/"target="_blank">Display</a></li>
+           
+		        <li><a class="nav-link scrollto" href="http://192.168.88.203/dashboard/FITUR/GRAFIK/sim/"target="_blank">Grafik</a></li>
+		  <li><a class="nav-link scrollto" href="http://192.168.88.203/dashboard/cetakdokumen.html" target="_blank">Cetak</a></li>
+		  <li><a class="nav-link scrollto" href="http://192.168.88.203/dashboard/DOWNLOAD-APLIKASI/"target="_blank">Download</a></li>
+          <li><a class="nav-link scrollto" href="http://192.168.88.203/dashboard/FITUR/PEMBAHARUAN.php"target="_blank">Update</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+
+    <div class="container">
+      <div class="row justify-content-between">
+        <div class="col-lg-7 pt-5 pt-lg-0 order-2 order-lg-1 d-flex align-items-center">
+          <div data-aos="zoom-out">
+            <h1>Selamat Datang <span>Aplikasi Versi 8.0</span></h1>
+            <h2>Pelayanan Cepat, Mudah & Aman</h2>
+            <div class="text-center text-lg-start">
+              <a href="http://192.168.88.203/dashboard/VERSI/LOADING-APLIKASI" class="btn-get-started scrollto">MULAI</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 order-1 order-lg-2 hero-img" data-aos="zoom-out" data-aos-delay="300">
+          <img src="assets/img/file.png" class="img-fluid animated" alt="">
+        </div>
+      </div>
+    </div>
+
+    <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28 " preserveAspectRatio="none">
+      <defs>
+        <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z">
+      </defs>
+      <g class="wave1">
+        <use xlink:href="#wave-path" x="50" y="3" fill="rgba(255,255,255, .1)">
+      </g>
+      <g class="wave2">
+        <use xlink:href="#wave-path" x="50" y="0" fill="rgba(255,255,255, .2)">
+      </g>
+      <g class="wave3">
+        <use xlink:href="#wave-path" x="50" y="9" fill="#fff">
+      </g>
+    </svg>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    
+         
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
