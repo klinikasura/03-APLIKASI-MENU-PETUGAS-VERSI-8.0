1a2e206d68ab6105fc43aac25a10c8a68e8a894b
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2024-08-04 14:54:40 +0700

    Add files via upload

---
+++ API-SYSTEM-KEAMANAN-DATABASE/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Aplikasi RS. Asura</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+ <link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" 
+
+ {
+          "resourceType": "OperationOutcome",
+          "issue": [
+            {
+              "severity": "AMAN",
+              "code": "valid-access-APLIKASI-RS-ASURA-V.8.0",
+              "details": {
+                "text": "valid-access-APLIKASI-RS-ASURA-V.8.0"
+              }
+            }
+          ]
+        }
+
+        
+</body>
+<p>
+<center>API-SYSTEM-KEAMANAN-APLIKASI</center>
+<p>
+<center> {
+          "resourceType": "OperationOutcome",
+          "issue": [
+            {
+              "severity": "AMAN",
+              "code": "valid-access-APLIKASI-RS-ASURA-V.8.0",
+              "details": {
+                "text": "valid-access-APLIKASI-RS-ASURA-V.8.0"
+              }
+            }
+          ]
+        }
+
+<p>
+<a href="http://192.168.88.203/dashboard/APLIKASI-SATUSEHAT/mlite-master/">APLIKASI-RS-ASURA-V.8.0</a>
+</html>
