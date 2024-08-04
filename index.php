efa656ad4861b822ef8526dbfcb7e81e999f7a4b
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2024-08-04 13:57:05 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,137 +1,101 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <meta name="description" content="">
-        <meta name="author" content="TemplateMo">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+	  <meta http-equiv="refresh" content="8;url=http://192.168.88.203/dashboard/index-master.php">   
+    <title>Aplikasi RS. Asura</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
 
-        <title>Aplikasi RS. Asura</title>
+  <!-- Favicons -->
 
-        <!-- CSS FILES -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        
-        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+   <link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+<audio autoplay>
+      <source src="http://192.168.88.203/dashboard/AUDIO/WELCOME.mp3" type="audio/mpeg">
+    </audio>
+      
 
-        <link href="http://192.168.88.203/dashboard/SISTEM8.0/css/bootstrap.min.css" rel="stylesheet">
 
-        <link href="http://192.168.88.203/dashboard/SISTEM8.0/css/bootstrap-icons.css" rel="stylesheet">
 
-        <link href="http://192.168.88.203/dashboard/SISTEM8.0/css/magnific-popup.css" rel="stylesheet">
 
-        <link href="http://192.168.88.203/dashboard/SISTEM8.0/css/templatemo-first-portfolio-style.css" rel="stylesheet">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="http://192.168.88.203/dashboard/INTERFACE-SYSTEM-RS/assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="http://192.168.88.203/dashboard/INTERFACE-SYSTEM-RS/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="http://192.168.88.203/dashboard/INTERFACE-SYSTEM-RS/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="http://192.168.88.203/dashboard/INTERFACE-SYSTEM-RS/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="http://192.168.88.203/dashboard/INTERFACE-SYSTEM-RS/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="http://192.168.88.203/dashboard/INTERFACE-SYSTEM-RS/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
      <link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
-	 
-	  <meta http-equiv="refresh" content="8;url=http://192.168.88.203/dashboard/APLIKASI-MOBILE/">    
-<!--
+  <!-- Template Main CSS File -->
+  <link href="http://192.168.88.203/dashboard/INTERFACE-SYSTEM-RS/assets/css/style.css" rel="stylesheet">
 
-TemplateMo 578 First Portfolio
+  <!-- =======================================================
+  * Template Name: Scaffold
+  * Template URL: https://bootstrapmade.com/scaffold-bootstrap-metro-style-template/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
 
-https://templatemo.com/tm-578-first-portfolio
+<body>
 
--->
-    </head>
-    
-    <body>
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center">
 
+      <div class="logo me-auto">
         
-        <main>
+    
+      </div>
+
+     
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center" data-aos="fade-up">
+          <div>
+            <h1>APLIKASI RS. ASURA</h1>
+            <h2>Integrasi SATU SEHAT</h2>
+           
+          </div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left">
+          <img src="http://192.168.88.203/dashboard/ROBOT-AI-SYSTEM.80-FINAL/assets/img/ROBOT.gif" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    
+  
+    
+      
 
 
+  <!-- Vendor JS Files -->
+  <script src="http://192.168.88.203/dashboard/INTERFACE-SYSTEM-RS/assets/vendor/aos/aos.js"></script>
+  <script src="http://192.168.88.203/dashboard/INTERFACE-SYSTEM-RS/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="http://192.168.88.203/dashboard/INTERFACE-SYSTEM-RS/assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="http://192.168.88.203/dashboard/INTERFACE-SYSTEM-RS/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="http://192.168.88.203/dashboard/INTERFACE-SYSTEM-RS/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="http://192.168.88.203/dashboard/INTERFACE-SYSTEM-RS/assets/vendor/php-email-form/validate.js"></script>
 
+  <!-- Template Main JS File -->
+  <script src="http://192.168.88.203/dashboard/INTERFACE-SYSTEM-RS/assets/js/main.js"></script>
 
+</body>
 
-            <section class="clients section-padding">
-                <div class="container">
-                    <div class="row align-items-center">
-
-                        <div class="col-lg-12 col-12">
-                            <h3 class="text-center mb-5">RS. ASURA </h3>
-                        </div>
-
-                        <div class="col-lg-2 col-4 ms-auto clients-item-height">
-                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/KEMENKES.jpg" class="clients-image img-fluid" alt="">
-                        </div>
-
-                        <div class="col-lg-2 col-4 clients-item-height">
-                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/ADMEDIKA.png" class="clients-image img-fluid" alt="">
-                        </div>
-
-                        <div class="col-lg-2 col-4 clients-item-height">
-                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/BPJS.jpg" class="clients-image img-fluid" alt="">
-                        </div>
-
-                        <div class="col-lg-2 col-4 clients-item-height">
-                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/bina.jpeg" class="clients-image img-fluid" alt="">
-                        </div>
-
-                        <div class="col-lg-2 col-4 me-auto clients-item-height">
-                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/INHEALTH.jpg" class="clients-image img-fluid" alt="">
-                        </div>
-						
-                        <div class="col-lg-2 col-4 me-auto clients-item-height">
-                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/SIMRS.jpeg" class="clients-image img-fluid" alt="">
-                        </div>
-						
-						 <div class="col-lg-2 col-4 me-auto clients-item-height">
-                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/BIO.jpg" class="clients-image img-fluid" alt="">
-                        </div>
-						
-						
-						 <div class="col-lg-2 col-4 me-auto clients-item-height">
-                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/BRI.jpg" class="clients-image img-fluid" alt="">
-                        </div>
-						
-						 <div class="col-lg-2 col-4 me-auto clients-item-height">
-                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/DEX.png" class="clients-image img-fluid" alt="">
-                        </div>
-						
-						<div class="col-lg-2 col-4 me-auto clients-item-height">
-                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/JAT.jpeg" class="clients-image img-fluid" alt="">
-                        </div>
-						
-						<div class="col-lg-2 col-4 me-auto clients-item-height">
-                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/KIM.jpg" class="clients-image img-fluid" alt="">
-                        </div>
-						
-						<div class="col-lg-2 col-4 me-auto clients-item-height">
-                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/SAN.png" class="clients-image img-fluid" alt="">
-                        </div>
-
-                    </div>
-                </div>
-            </section>
-
-
-         
-        </main>
-
-    <footer class="site-footer">
-            <div class="container">
-                <div class="row">
-
-                    <div class="col-lg-12 col-12">
-                        <div class="copyright-text-wrap">
-                            <p align="center" class="mb-0">
-                                <span class="copyright-text">Copyright © Aplikasi RS. Asura Versi 8.0 <a href=""></a></span>                            </p>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </footer>
-
-        <!-- JAVASCRIPT FILES -->
-        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/jquery.min.js"></script>
-        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/bootstrap.min.js"></script>
-        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/jquery.sticky.js"></script>
-        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/click-scroll.js"></script>
-        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/jquery.magnific-popup.min.js"></script>
-        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/magnific-popup-options.js"></script>
-        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/custom.js"></script>
-
-    </body>
 </html>
