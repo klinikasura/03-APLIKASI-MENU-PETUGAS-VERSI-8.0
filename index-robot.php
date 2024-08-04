2e28e49bdf6422a03bd1bc56dfc32e7b1cccedb6
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2024-08-04 14:16:27 +0700

    Add files via upload

---
+++ index-robot.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="">
+        <meta name="author" content="TemplateMo">
+
+        <title>Aplikasi RS. Asura</title>
+
+        <!-- CSS FILES -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+
+        <link href="http://192.168.88.203/dashboard/SISTEM8.0/css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="http://192.168.88.203/dashboard/SISTEM8.0/css/bootstrap-icons.css" rel="stylesheet">
+
+        <link href="http://192.168.88.203/dashboard/SISTEM8.0/css/magnific-popup.css" rel="stylesheet">
+
+        <link href="http://192.168.88.203/dashboard/SISTEM8.0/css/templatemo-first-portfolio-style.css" rel="stylesheet">
+     <link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+	 
+
+
+    </head>
+    
+    <body>
+
+        
+        <main>
+
+
+
+
+
+            <section class="clients section-padding">
+                <div class="container">
+                    <div class="row align-items-center">
+
+                        <div class="col-lg-12 col-12">
+                            <h3 class="text-center mb-5">SEDANG MAINTETANCE - INTEGRASI SATU SEHAT - KEMBALI NORMAL PADA TANGGAL 1 AGUSTUS 2024 ------------- :) ---------------------------------- ROBOT AI SYSTEM 8.0           </h3> <h3 class="text-center mb-5">PROSES UPGRADE SUPERHERO APLIKASI 50%         </h3>
+                        </div>
+
+                        
+        <!-- JAVASCRIPT FILES -->
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/jquery.min.js"></script>
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/bootstrap.min.js"></script>
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/jquery.sticky.js"></script>
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/click-scroll.js"></script>
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/jquery.magnific-popup.min.js"></script>
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/magnific-popup-options.js"></script>
+        <script src="http://192.168.88.203/dashboard/SISTEM8.0/js/custom.js"></script>
+
+    </body>
+</html>
