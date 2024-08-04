359e7f34fb3e719dfb38779d59ca20091d7fc74f
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2024-08-04 14:44:18 +0700

    Add files via upload

---
+++ KODE-ICD/index-mobile.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
+  <title>Aplikasi RS. Asura</title>
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="viewport" content="width=device-width">        
+  <link rel="stylesheet" href="css/templatemo_main.css">
+  	<link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+<!-- 
+Dashboard Template 
+http://www.templatemo.com/preview/templatemo_415_dashboard
+-->
+</head>
+<body>
+  <div class="navbar navbar-inverse" role="navigation">
+      <div class="navbar-header">
+        <div class="logo"><h1>Dashboard Administrasi</h1></div>
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button> 
+      </div>   
+    </div>
+    <div class="template-page-wrapper">
+      <div class="navbar-collapse collapse templatemo-sidebar">
+        <ul class="templatemo-sidebar-menu">
+          <li>
+            
+          </li>
+          <li class="active"><a href="index.php"><i class="fa fa-home"></i>Dashboard</a></li>
+          <li class="sub open">
+            <a href="javascript:;">
+              <i class="fa fa-database"></i> Data Poliklinik<div class="pull-right"><span class="caret"></span></div>
+            </a>
+            <ul class="templatemo-submenu">
+              <li><a href="http://192.168.88.203/webapps/poli.php"target="frame">Data Pasien Hari Ini</a></li>
+     
+	   
+	   
+	    <li><a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/POLIKLINIK/JADWALDOKTER/"target="frame">Jadwal Praktek Dokter</a></li>
+		
+		<li><a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/POLIKLINIK/sim/"target="frame">Grafik Poliklinik</a></li>
+		
+		<li><a href="http://192.168.88.203/dashboard/DAFTAR-PASIEN/"target="frame">Layanan Poliklinik</a></li>
+		
+		
+
+                
+            </ul>
+          </li>
+          
+         
+          </li>
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		   <li class="sub open">
+            <a href="javascript:;">
+              <i class="fa fa-database"></i> BPJS <div class="pull-right"><span class="caret"></span></div>
+            </a>
+            <ul class="templatemo-submenu">
+              
+       <li><a href="https://pcarejkn.bpjs-kesehatan.go.id/eclaim/Login"target="blank">PCARE</a></li>
+	   
+	   
+	   
+	        <li><a href="https://antrean.bpjs-kesehatan.go.id/antrean-faskes/#/access/signin"target="blank">Antrian Online</a></li>
+	  
+	  
+	  
+	       <li><a href="https://hfis.bpjs-kesehatan.go.id/hfis/login"target="blank">HFIS</a></li>
+	  
+	  
+	       <li><a href="https://api.whatsapp.com/send/?phone=6208118750400&text&type=phone_number&app_absent=0"target="blank">Cek JKN-KIS</a></li>
+	  
+	   
+	   
+	   
+	   
+	   
+
+ 
+            </ul>
+          </li>
+		  
+		  
+		  
+		  
+		     <li class="sub open">
+            <a href="javascript:;">
+              <i class="fa fa-database"></i> Informasi <div class="pull-right"><span class="caret"></span></div>
+            </a>
+            <ul class="templatemo-submenu">
+              
+       <li><a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/POLIKLINIK/cekjadwaloperasi.php"target="frame">Jadwal Operasi</a></li>
+	   
+	   
+	   
+	        <li><a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/POLIKLINIK/CEK-BIAYA/cekoperasi.php"target="frame">Biaya Operasi</a></li>
+	  
+	  
+	  
+	  <li><a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/POLIKLINIK/CEK-BIAYA/ceklab.php"target="frame">Biaya Laboratorium</a></li>
+	  
+	  
+	  <li><a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/POLIKLINIK/CEK-BIAYA/cekradiologi.php"target="frame">Biaya Radiologi</a></li>
+	  
+	  
+	  
+	       <li><a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/POLIKLINIK/KETERSEDIAAN/cekranap.php"target="frame">Cek Kamar Inap</a></li>
+	  
+	  
+	       <li><a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/POLIKLINIK/KETERSEDIAAN/cekdarah.php"target="frame">Cek Stok Darah</a></li>
+	  
+	  
+	       <li><a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/POLIKLINIK/ANTRIAN-POLI/anjungan/poli"target="frame">Antrian Poliklinik</a></li>
+	   
+	   
+	   
+	   
+	     <li><a href="http://192.168.88.203/dashboard/APLIKASI/epasien/"target="frame">Epasien</a></li>
+	   
+	   
+	   
+	      
+	     <li><a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/DISPLAY-TV/TV/anjungan/loket?show=panggil_loket"target="frame">Antrian Loket</a></li>
+	 
+	 
+	    
+	     <li><a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/DISPLAY-TV/TV/anjungan/loket?show=panggil_cs"target="frame">Antrian Apotik</a></li>
+	 
+	   
+
+ 
+            </ul>
+          </li>
+		  
+		  
+		   <li class="sub open">
+            <a href="javascript:;">
+              <i class="fa fa-database"></i>Menu Petugas <div class="pull-right"><span class="caret"></span></div>
+            </a>
+            <ul class="templatemo-submenu">
+              
+       <li><a href="http://192.168.88.203/dashboard/APLIKASI/KHANZALITE-PAKESUARA/admin/rawat_jalan/manage?t=3616bfc1931d"target="frame">Daftar Pasien</a></li>
+	     <li><a href="http://192.168.88.203/dashboard/APLIKASI/KHANZALITE-PAKESUARA/admin/rawat_inap/manage?t=3616bfc1931d"target="frame">Daftar Rawat Inap</a></li>
+	   
+	   
+
+ 
+            </ul>
+          </li>
+          
+		  
+		  
+		  
+		  
+		  
+  
+          <li class="sub open">
+            <a href="javascript:;">
+              <i class="fa fa-database"></i>Cetak <div class="pull-right"><span class="caret"></span></div>
+            </a>
+            <ul class="templatemo-submenu">
+              
+       <li><a href="http://192.168.88.203/dashboard/pendaftaran.html"target="frame">Cetak Dokumen</a></li>
+	     <li><a href="https://u.pcloud.link/publink/show?code=kZcv1SVZFy8hPkWi5n86R0EwBaAh7jVC3loX"target="frame">Cetak Dokumen Online</a></li>
+	   
+	   
+
+ 
+            </ul>
+          </li>
+          
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+          <li><a href="http://192.168.88.203/dashboard/"  data-target="index.php"><i class="fa fa-sign-out"></i>Logout</a></li>
+        </ul>
+      </div><!--/.navbar-collapse -->
+
+      <div class="templatemo-content-wrapper">
+        <div class="templatemo-content">
+      
+   <iframe src="http://192.168.88.203/dashboard/MENU-ADMIN/ADMIN.jpg" name="frame" width="100%" height="1800px" allowtransparency="true" noresize="noresize"></iframe>
+          </ol>
+          <div class="margin-bottom-30">
+            <div class="row"></div>
+          </div>         
+
+          <div class="row"></div>
+          <div class="templatemo-panels">
+            <div class="row"></div>
+            <div class="row"></div>
+          </div>    
+        </div>
+      </div>
+      <!-- Modal -->
+      <div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+              <h4 class="modal-title" id="myModalLabel">Are you sure you want to sign out?</h4>
+            </div>
+            <div class="modal-footer">
+              <a href="sign-in.html" class="btn btn-primary">Yes</a>
+              <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer class="templatemo-footer">
+        <div class="templatemo-copyright">
+          <div class="container"></div>
+        </div>
+      
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <p>Copyright &copy; RS. Asura 
+          
+          <br>
+          Aplikasi Versi 8.0 </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+      </footer>
+    </div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/Chart.min.js"></script>
+    <script src="js/templatemo_script.js"></script>
+    <script type="text/javascript">
+    // Line chart
+    var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
+    var lineChartData = {
+      labels : ["January","February","March","April","May","June","July"],
+      datasets : [
+      {
+        label: "My First dataset",
+        fillColor : "rgba(220,220,220,0.2)",
+        strokeColor : "rgba(220,220,220,1)",
+        pointColor : "rgba(220,220,220,1)",
+        pointStrokeColor : "#fff",
+        pointHighlightFill : "#fff",
+        pointHighlightStroke : "rgba(220,220,220,1)",
+        data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+      },
+      {
+        label: "My Second dataset",
+        fillColor : "rgba(151,187,205,0.2)",
+        strokeColor : "rgba(151,187,205,1)",
+        pointColor : "rgba(151,187,205,1)",
+        pointStrokeColor : "#fff",
+        pointHighlightFill : "#fff",
+        pointHighlightStroke : "rgba(151,187,205,1)",
+        data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+      }
+      ]
+
+    }
+
+    window.onload = function(){
+      var ctx_line = document.getElementById("templatemo-line-chart").getContext("2d");
+      window.myLine = new Chart(ctx_line).Line(lineChartData, {
+        responsive: true
+      });
+    };
+
+    $('#myTab a').click(function (e) {
+      e.preventDefault();
+      $(this).tab('show');
+    });
+
+    $('#loading-example-btn').click(function () {
+      var btn = $(this);
+      btn.button('loading');
+      // $.ajax(...).always(function () {
+      //   btn.button('reset');
+      // });
+  });
+  </script>
+</body>
+</html>
---
+++ KODE-ICD/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Aplikasi RS. Asura</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+ <link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+ <meta http-equiv="refresh" content="40;url=http://192.168.88.203/dashboard/TAMPILAN-APLIKASI/TEMPLATE-APLIKASI">  
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Roboto:100,300,400,500,700|Philosopher:400,400i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+	
+
+  <!-- =======================================================
+  * Template Name: eStartup
+  * Template URL: https://bootstrapmade.com/estartup-bootstrap-landing-page-template/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<audio autoplay>
+      <source src="http://192.168.88.203/dashboard/AUDIO/ICD.mp3" type="audio/mpeg">
+       </audio>
+ 
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div id="logo">
+       
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" title="" /></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+       
+          
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container" data-aos="fade-in">
+
+      <h2></h2>
+      <img src="assets/img/ROBOT.gif" alt="Hero Imgs" data-aos="zoom-out" data-aos-delay="100">
+      <a href="http://192.168.88.203/dashboard/KODE-ICD/index-kode-icd.php" class="btn-get-started scrollto">Mulai</a>
+      <div class="btns">
+
+    </div>
+  </section><!-- End Hero Section -->
+
+  <main id="main">
+
+    
+
+           
+          
+    <center><div class="copyrights">
+      <div class="container">
+ 
+        <div class="credits">
+          
+         
+        </div>
+      </div>
+    </div>
+
+  </footer><!-- End  Footer -->
+
+
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
