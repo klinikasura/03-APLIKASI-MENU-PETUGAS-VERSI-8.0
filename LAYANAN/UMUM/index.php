020c780e1bed16bb939e72bbe009432ea6d0d4d4
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2024-08-04 14:30:40 +0700

    Add files via upload

---
+++ LAYANAN/UMUM/index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+
+
+<head>
+
+  <meta charset="utf-8">
+
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+
+
+  <title>Aplikasi RS. Asura</title>
+
+  <meta content="" name="description">
+
+  <meta content="" name="keywords">
+
+
+
+  <!-- Favicons -->
+
+<link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+
+
+
+  <!-- Google Fonts -->
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+
+
+  <!-- Vendor CSS Files -->
+
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+
+
+  <!-- Template Main CSS File -->
+
+  <link href="assets/css/style.css" rel="stylesheet">
+
+
+
+  <!-- =======================================================
+
+  * Template Name: Vesperr
+
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
+
+  * Author: BootstrapMade.com
+
+  * License: https://bootstrapmade.com/license/
+
+  ======================================================== -->
+
+</head>
+
+
+
+<body>
+
+
+
+  <!-- ======= Header ======= -->
+
+  <header id="header" class="fixed-top d-flex align-items-center">
+
+    <div class="container d-flex align-items-center justify-content-between">
+
+
+
+      <div class="logo">
+
+     
+
+        <!-- Uncomment below if you prefer to use an image logo -->
+
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      </div>
+
+
+
+      <nav id="navbar" class="navbar">
+
+
+
+
+
+
+
+
+    <!-- ======= Features Section ======= -->
+
+    <section id="features" class="features">
+
+      <div class="container">
+
+
+
+        <div class="section-title" data-aos="fade-up">
+
+          <h2>LAYANAN UMUM </h2>
+
+          <p>Pilih Layanan </p>
+
+        </div>
+
+
+
+        <div class="row" data-aos="fade-up" data-aos-delay="300">
+
+          <div class="col-lg-3 col-md-4">
+
+            <div class="icon-box">
+
+               <i class="ri-bar-chart-box-line" style="color: #5578ff;"></i>
+
+              <h3><a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/CETAK-ANTRIAN/index-cetakantrian.php" target="_blank">Cetak Antrian </a></h3>
+
+            </div>
+
+          </div>
+
+          <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+
+            <div class="icon-box">
+
+              <i class="ri-bar-chart-box-line" style="color: #5578ff;"></i>
+
+              <h3><a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/DAFTAR-PASIEN/index-daftarpasien.php" target="_blank">Daftar Pasien </a></h3>
+
+            </div>
+
+          </div>
+
+          <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+
+            <div class="icon-box">
+  <i class="ri-bar-chart-box-line" style="color: #5578ff;"></i>
+              <h3><a href="http://192.168.88.203/dashboard/APLIKASI-APS.6.0/POLIKLINIK/index-poliklinik.php" target="_blank">Poliklinik </a></h3>
+
+            </div>
+
+          </div>
+
+          <div class="col-lg-3 col-md-4 mt-4 mt-lg-0">
+
+            <div class="icon-box">
+
+               <i class="ri-bar-chart-box-line" style="color: #5578ff;"></i>
+              <h3><a href="http://192.168.88.203/dashboard/cetakdokumen.html" target="_blank">Cetak Dokumen </a></h3>
+
+            </div>
+
+          </div>
+
+          <div class="col-lg-3 col-md-4 mt-4">
+
+            <div class="icon-box">
+
+               <i class="ri-bar-chart-box-line" style="color: #5578ff;"></i>
+              <h3><a href="">Bayar QRIS BRI</a></h3>
+
+            </div>
+
+          </div>
+
+          <div class="col-lg-3 col-md-4 mt-4">
+
+            <div class="icon-box">
+
+              <i class="ri-bar-chart-box-line" style="color: #5578ff;"></i>
+
+              <h3><a href="https://docs.google.com/forms/d/e/1FAIpQLSc_oLWzCARv4PJY69d_Yymq2QOM-f8XWaJFjgVQZjpOsDoFKQ/viewform" target="_blank">Kritik & Saran </a></h3>
+
+            </div>
+
+          </div>
+
+          <div class="col-lg-3 col-md-4 mt-4">
+
+            
+
+            </div>
+
+          </div>
+
+        </div>
+
+
+
+      </div>
+
+    </section><!-- End Features Section -->
+
+
+
+    
+    
+
+
+  <!-- Vendor JS Files -->
+
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+
+
+  <!-- Template Main JS File -->
+
+  <script src="assets/js/main.js"></script>
+
+
+
+</body>
+
+
+
+</html>
