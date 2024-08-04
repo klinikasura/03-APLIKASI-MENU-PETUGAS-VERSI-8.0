9dcf3096b57db1c9ffb327b35010c2a6cf4d0eb1
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2024-08-04 14:21:02 +0700

    Add files via upload

---
+++ TAMPILAN-APLIKASI/index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+ <title>Aplikasi RS. Asura</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+    <link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" 
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Scaffold
+  * Template URL: https://bootstrapmade.com/scaffold-bootstrap-metro-style-template/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+
+    <!-- ======= Team Section ======= -->
+         <center>  <section id="team" class="team">
+            <center> <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>APLIKASI RS. ASURA</h2>
+          <p>UPGRADE APLIKASI SUPER HERO AI ROBOT SYSTEM V.8.0</p>
+        </div>
+
+      <center>  <div class="row">
+
+        <div class="col-lg-4 col-md-6">
+            <div class="member" data-aos="zoom-in">
+              <div class="pic">
+                <div align="center"><a href="http://192.168.88.203/dashboard/VERSI/LOADING/"><img src="http://192.168.88.203/dashboard/download.jpeg" alt="" border="0" class="img-fluid"></a></div>
+              </div>
+              <div class="member-info">
+                <h4 align="center">BERANDA</h4>
+                <div align="center"><span>Dashboard Tampilan Administrator</span>
+                  
+                  </div>
+              </div>
+            </div>
+          </div>
+   <div class="col-lg-4 col-md-6">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic"> 
+                <div align="center"><a href="http://192.168.88.203/dashboard/ROBOT-AI-SYSTEM.80-FINAL/"><img src="http://192.168.88.203/dashboard/ROBOT-AI-SYSTEM.80-FINAL/assets/img/ROBOT.gif" alt="" border="0" class="img-fluid"></a></div>
+              </div>
+              <div class="member-info">
+                <h4 align="center">APLIKASI</h4>
+                <div align="center"><span>SOAP,SATU SEHAT, APLIKASI DOKTER, PASIEN, DLL</span>
+                  
+
+
+
+                  </div>
+              </div>
+            </div>
+          </div>
+   <div class="col-lg-4 col-md-6">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+              <div class="pic"> 
+                <div align="center"><a href="http://192.168.88.203/dashboard/TAMPILAN-APLIKASI/DOWNLOAD-APK/" target="_blank"><img src="http://192.168.88.203/dashboard/TAMPILAN-APLIKASI/SUPER.gif" alt="" border="0" class="img-fluid"></a></div>
+              </div>
+              <div class="member-info">
+                <h4 align="center">DOWNLOAD APLIKASI</h4>
+                <div align="center"><span>BERIKAN KEMUDAHAN DALAM SATU GENGGAMAN</span>
+                  
+
+                  </div>
+              </div>
+            </div>
+          </div>
+
+
+
+   
+
+        </div>
+
+      </div>
+    </section><!-- End Team Section -->
+
+
+
+
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
