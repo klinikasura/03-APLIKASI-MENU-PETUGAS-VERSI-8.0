3f20cff0665c99b6d2cf426ac729e812fbbdd144
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2024-08-04 14:24:36 +0700

    Add files via upload

---
+++ RMKLINIK/APLIKASI/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Aplikasi RS. Asura</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+	<link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Vesperr
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+
+    </section>
+    <p>
+      <!-- End Testimonials Section -->
+      
+      <!-- ======= Portfolio Section ======= -->
+</p>
+    <p>&nbsp;    </p>
+<div class="section-title" data-aos="fade-up">
+  <h2>rm klinik asura </h2>
+</div>
+
+       
+
+        <ul>
+          <li><a href="http://192.168.88.203/dashboard/RMKLINIK/LEMBAR%20SKRINING%20RAWAT%20JALAN%20KLINIK%20ASURA%20EDIT%20FIX.pdf" target="_blank">LEMBAR SKRINING RAWAT JALAN KLINIK ASURA EDIT FIX</a></li>
+          <li><a href="http://192.168.88.203/dashboard/RMKLINIK/KAJIAN%20AWAL%20RAWAT%20JALAN%20PUSKESMAS%20TUGUMULYO%20UNTUK%20KLINIK.pdf" target="_blank">KAJIAN AWAL RAWAT JALAN PUSKESMAS TUGUMULYO UNTUK KLINIK</a></li>
+          <li><a href="http://192.168.88.203/dashboard/RMKLINIK/GENERAL%20CONSENT%20KLINIK%20ASURA.pdf" target="_blank">GENERAL CONSENT KLINIK ASURA</a></li>
+          <li><a href="http://192.168.88.203/dashboard/RMKLINIK/FORM%20SURGICAL%20SAFETY%20CHECKLIST%20KLINIK%20ASURA.pdf" target="_blank">FORM SURGICAL SAFETY CHECKLIST KLINIK ASURA</a></li>
+          <li><a href="http://192.168.88.203/dashboard/RMKLINIK/FORM%20PENANDAAN%20LOKASI%20OPERASI%20KLINIK%20ASURA.pdf" target="_blank">FORM PENANDAAN LOKASI OPERASI KLINIK ASURA</a></li>
+          <li><a href="http://192.168.88.203/dashboard/RMKLINIK/FORM%20INFORMED%20CONSENT%20BENAR%20KLINIK%20ASURA.pdf" target="_blank">FORM INFORMED CONSENT BENAR KLINIK ASURA</a></li>
+          <li><a href="http://192.168.88.203/dashboard/RMKLINIK/CPPT%20VERSI%20BARU%202023%20KLINIK%20ASURA%20EDIT.pdf" target="_blank">CPPT VERSI BARU 2023 KLINIK ASURA EDIT</a></li>
+          <li><a href="http://192.168.88.203/dashboard/RMKLINIK/ceklis%20persiapan%20rujukan%20KLINIK%20ASURA.pdf" target="_blank">ceklis persiapan rujukan KLINIK ASURA</a></li>
+          <li><a href="http://192.168.88.203/dashboard/RMKLINIK/catatan%20Anestesi%20KLINIK%20ASURA.pdf" target="_blank">catatan Anestesi KLINIK ASURA</a></li>
+          <li><a href="http://192.168.88.203/dashboard/RMKLINIK/KAJIAL%20AWAL%20IBU%20HAMIL%20KLINIK%20ASURA.pdf" target="_blank">KAJIAL AWAL IBU HAMIL KLINIK ASURA</a></li>
+          <li><a href="http://192.168.88.203/dashboard/RMKLINIK/REKAM%20MEDIS%20UGD%20KLINIK%20ASURA%20HALAMAN%2002%20fix%20.pdf" target="_blank">REKAM MEDIS UGD KLINIK ASURA HALAMAN 02 fix </a></li>
+          <li><a href="http://192.168.88.203/dashboard/RMKLINIK/REKAM%20MEDIS%20UGD%20KLINIK%20ASURA%20HALAMAN%2001%20fix%20.pdf" target="_blank">REKAM MEDIS UGD KLINIK ASURA HALAMAN 01 fix </a></li>
+          <li><a href="http://192.168.88.203/dashboard/RMKLINIK/CETAK%20REKAM%20MEDIS%20KLINIK%20ASURA%20FULL%20F4.pdf" target="_blank">CETAK REKAM MEDIS KLINIK ASURA FULL F4</a></li>
+        </ul>
+        <p align="center">&nbsp;</p>
+        <p align="center">&nbsp;</p>
+        <p align="center">&nbsp;</p>
+        <p align="center">&nbsp;</p>
+        <p align="center">&nbsp;</p>
+        <p align="center">&nbsp;</p>
+        <p>
+          <!-- Vendor JS Files -->
+          <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+          <script src="assets/vendor/aos/aos.js"></script>
+          <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+          <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+          <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+          <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+          <script src="assets/vendor/php-email-form/validate.js"></script>
+          
+          <!-- Template Main JS File -->
+          <script src="assets/js/main.js"></script>
+                        </p>
+</body>
+
+</html>
