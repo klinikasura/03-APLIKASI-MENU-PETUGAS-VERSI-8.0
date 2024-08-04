dabef59016317551b76ab5f96a17b0a02a8c9529
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2024-08-04 14:29:48 +0700

    Add files via upload

---
+++ MASTER-APLIKASI/E-FARMASI/index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+        <title>Aplikasi RS. Asura</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+
+    <link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+ <meta http-equiv="refresh" content="40;url=http://192.168.88.203/dashboard/TAMPILAN-APLIKASI/TEMPLATE-APLIKASI">  
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Marcellus:wght@400&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+
+
+
+
+
+ 
+<audio autoplay>
+      <source src="http://192.168.88.203/dashboard/AUDIO/FARMASI.mp3" type="audio/mpeg">
+    </audio>
+ 
+
+
+    
+
+    <p>
+
+
+
+<center><h1>APLIKASI RS. ASURA</h1></center>
+    
+
+    <!-- Services Section -->
+    <section id="services" class="services section">
+
+      <!-- Section Title -->
+      <div class="container section-title" data-aos="fade-up">
+        
+      </div><!-- End Section Title -->
+      <div class="content">
+        <div class="container">
+          <div class="row g-0">
+            <div class="col-lg-3 col-md-6">
+              <div class="service-item">
+                <span class="number">01</span>
+                <div class="service-item-icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="80" height="80" x="0" y="0" viewBox="0 0 514.314 514.314" style="enable-background: new 0 0 512 512" xml:space="preserve" class="">
+                    <g> </g>
+                  </svg>
+                <a href="http://192.168.88.203/dashboard/APLIKASI-SATUSEHAT/mlite-master/APLIKASI-SOAP/" target="_blank"><img src="http://192.168.88.203/dashboard/APLIKASI-SATUSEHAT/mlite-master/assets/img/OBAT.jpg" border="0" href="http://192.168.88.203/dashboard/APLIKASI-SATUSEHAT/mlite-master/APLIKASI-SOAP/" ></a></div>
+                <div class="service-item-content">
+                  <h3 class="service-heading">E-FARMASI</h3>
+                  <p>
+                    Koneksi Database                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+              <div class="service-item">
+                <span class="number">02</span>
+                <div class="service-item-icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="80" height="80" x="0" y="0" viewBox="0 0 514.314 514.314" style="enable-background: new 0 0 512 512" xml:space="preserve" class="">
+                    <g>                    </g>
+                  </svg>
+                  <a href="http://192.168.88.203/dashboard/APLIKASI-FARMASI/DATAOBAT/" target="_blank"><img src="http://192.168.88.203/dashboard/APLIKASI-SATUSEHAT/mlite-master/assets/img/OBAT.jpg" border="0" href="http://192.168.88.203/dashboard/APLIKASI-FARMASI/DATAOBAT/" ></a></div>
+                <div class="service-item-content">
+                  <h3 class="service-heading">E-FARMASI (Cek Harga Obat)</h3>
+                  <p> Koneksi Lokal </p>
+                </div>
+              </div>
+            </div>
+            
+            </div>
+
+     
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section><!-- /Services Section -->
+
+   
+        
+      </div>
+    </div>
+
+  </footer>
+
+ 
+
+
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+
+  <!-- Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
