af3fc35d1e1ff9fb3b35c4791d021b27e68742d4
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2024-08-04 14:28:25 +0700

    Add files via upload

---
+++ OK/index.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Aplikasi RS. Asura</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+ 
+<link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Vesperr
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+  <audio autoplay>
+      <source src="http://192.168.88.203/dashboard/RS-ASURA/SUARA.mp3" type="audio/mpeg">
+    </audio>
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+
+<!-- ======= Header ======= -->
+
+  <header id="header" class="fixed-top d-flex align-items-center">
+
+    <div class="container d-flex align-items-center justify-content-between">
+
+
+
+      
+
+         <div class="logo">
+        <h1><a href="">RS. Asura</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+
+
+
+      <nav id="navbar" class="navbar">
+
+        <ul>
+
+          <li><a class="nav-link scrollto active" href="http://192.168.88.203/dashboard">Beranda</a></li>
+		  
+		   <li><a class="nav-link scrollto" href="http://192.168.88.203/dashboard/LAYANAN/ANJUNGAN/pendaftaran.php" target="_blank">Register</a></li>
+		  
+          <li><a class="nav-link scrollto" href="http://192.168.88.203/dashboard/RMKLINIK/APLIKASI" target="_blank">RM</a></li>
+		  
+
+          <li><a class="nav-link scrollto" href="http://192.168.88.203/dashboard/LAYANAN/AKREDITASI" target="_blank">Akreditasi</a></li>
+		  
+		   <li><a class="nav-link scrollto" href="http://192.168.88.203/dashboard/LAYANAN/LAPORAN/" target="_blank">Laporan</a></li>
+
+ <li><a class="nav-link scrollto" href="http://192.168.88.203/dashboard/KARYAWAN" target="_blank">Karyawan</a></li>
+
+         
+<li><a class="nav-link scrollto" href="http://192.168.88.203/dashboard/VERSI/">Tampilan</a></li>
+
+           
+
+          
+
+          <li><a class="getstarted scrollto" href="http://192.168.88.203/dashboard/OKEY/APLIKASI/OK.apk">Download</a></li>
+
+        </ul>
+
+        <i class="bi bi-list mobile-nav-toggle"></i>
+
+      </nav><!-- .navbar -->
+
+
+
+    </div>
+
+  </header><!-- End Header -->
+
+
+
+
+      <div class="logo">
+        <h1><a href="">Aplikasi RS. Asura</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+          <h1 data-aos="fade-up">Melayani Dengan Sepenuh Hati</h1>
+          <h2 data-aos="fade-up" data-aos-delay="400">Ada Yang Bisa Kami Bantu ?</h2>
+          <div data-aos="fade-up" data-aos-delay="800">
+            <a href="http://192.168.88.203/dashboard/LAYANAN/UMUM" target="_blank" class="btn-get-started scrollto">Layanan Umum</a>
+          </div>
+		  
+		  <div data-aos="fade-up" data-aos-delay="800">
+            <a href="http://192.168.88.203/dashboard/LAYANAN/BPJS" target="_blank" class="btn-get-started scrollto">Layanan BPJS</a>
+          </div>
+		  
+		  <div data-aos="fade-up" data-aos-delay="800">
+            <a href="http://192.168.88.203/dashboard/LAYANAN/ASURANSI/" target="_blank" class="btn-get-started scrollto">Layanan Asuransi</a>          </div>
+			
+			
+		  
+		  <div data-aos="fade-up" data-aos-delay="800">
+            <a href="http://192.168.88.203/dashboard/HELPDESKIT.html" target="_blank" class="btn-get-started scrollto">Helpdesk IT</a>          </div>
+		  
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left" data-aos-delay="200">
+          <img src="assets/img/hero-img.png" class="img-fluid animated" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Clients Section ======= -->
+    <section id="clients" class="clients clients">
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-lg-2 col-md-4 col-6">
+            <p><a href="http://192.168.88.203/dashboard/MENU-ADMIN/MOBILE/index.php" target="_blank"><img src="assets/img/DAFTAR.png" alt="" border="0" class="img-fluid" data-aos="zoom-in"></a> </p>
+            <p><a href="http://192.168.88.203/dashboard/MENU-ADMIN/MOBILE/index.php" target="_blank">ADMIN</a></p>
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6">
+            <p><a href="http://192.168.88.203/dashboard/APLIKASI/dokter/login.php" target="_blank"><img src="assets/img/DOKTER.png" alt="" width="512" height="512" border="0" class="img-fluid" href="#about" data-aos="zoom-in" data-aos-delay="100"></a> </p>
+            <p><a href="http://192.168.88.203/dashboard/APLIKASI/dokter/login.php" target="_blank">DOKTER</a></p>
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6">
+            <p><a href="http://192.168.88.203/dashboard/APLIKASI/KHANZALITE-PAKESUARA/" target="_blank"><img src="assets/img/MEDIS.png" alt="" width="512" height="512" border="0" class="img-fluid" data-aos="zoom-in" data-aos-delay="200"></a> </p>
+            <p><a href="http://192.168.88.203/dashboard/APLIKASI/KHANZALITE-PAKESUARA/" target="_blank">PARAMEDIS</a></p>
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6">
+            <p><a href="http://192.168.88.203/dashboard/MENU-APOTIK/MOBILE/index.php" target="_blank"><img src="assets/img/FARMASI.png" alt="" width="512" height="512" border="0" class="img-fluid" data-aos="zoom-in" data-aos-delay="300"></a> </p>
+            <p><a href="http://192.168.88.203/dashboard/MENU-APOTIK/MOBILE/index.php" target="_blank">FARMASI</a></p>
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6">
+            <p><a href="http://192.168.88.203/dashboard/MENU-KEUANGAN/MOBILE/index.php" target="_blank"><img src="assets/img/KEUANGAN.png" alt="" width="376" height="512" border="0" class="img-fluid" data-aos="zoom-in" data-aos-delay="400"></a> </p>
+            <p><a href="http://192.168.88.203/dashboard/MENU-KEUANGAN/MOBILE/index.php" target="_blank">KEUANGAN</a></p>
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6">
+            <p><a href="http://192.168.88.203/dashboard/MENU-MANAGEMENT/MOBILE/index.php" target="_blank"><img src="assets/img/IT.png" alt="" width="512" height="512" border="0" class="img-fluid" data-aos="zoom-in" data-aos-delay="500"></a> </p>
+            <p><a href="http://192.168.88.203/dashboard/MENU-MANAGEMENT/MOBILE/index.php" target="_blank">IT</a></p>
+          </div>
+
+        </div>
+
+      </div>
+   
+
+ 
+
+           
+   <!-- ======= Footer ======= -->
+
+  <footer id="footer">
+
+    <div class="container">
+
+      <div class="row d-flex align-items-center">
+
+
+          <div class="copyright">
+
+            Copyright © Aplikasi RS. Asura Versi 8.0</strong>
+
+          </div>
+
+          <div class="credits">
+
+            <!-- All the links in the footer should remain intact. -->
+
+            <!-- You can delete the links only if you purchased the pro version. -->
+
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/vesperr-free-bootstrap-template/ -->
+
+      
+
+          </div>
+
+        </div>
+
+        
+
+      </div>
+
+    </div>
+
+  </footer><!-- End Footer -->
+
+
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+
+</html>
