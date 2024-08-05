b2f50fbbcc4d25e5c2c0e206b4a5a571f41f7fa2
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2024-08-05 12:02:28 +0700

    Add files via upload

---
+++ index.sql
@@ -0,0 +1,11 @@
+#include <iostream>[[Image:Create a Simple Program in C++ Step 1 Version 3.jpg|center]]
+#include <string> 
+using namespace std; 
+int main () 
+{
+	string s;
+	cout << "Nama Anda \n";
+	cin  >> s;
+	cout << "Halo, " << s << '\n' ;
+	return 0;
+}
