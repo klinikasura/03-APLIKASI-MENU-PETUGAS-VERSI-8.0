5702a5c6aa9d03132c681eaec3bf71644e6864f0
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2024-08-10 11:32:50 +0700

    Add files via upload

---
+++ UPDATE-ROBOT-8.0/index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+<title>Aplikasi RS. Asura</title>
+
+
+  <!-- Favicons -->
+   <link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+  <!-- Fonts -->
+
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+
+<audio autoplay>
+      <source src="http://192.168.88.203/dashboard/AUDIO/UPDATE-NEW.mp3" type="audio/mpeg">
+    </audio>
+
+
+  <!-- =======================================================
+  * Template Name: Moderna
+  * Template URL: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/
+  * Updated: Aug 07 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+
+        
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+    
+
+
+    <section id="featured-services" class="featured-services section" >
+<center><p> BELUM UPDATE YA, UPDATE DULU, DENGAN CARA DOWNLOAD APLIKASI VERSI TERBARU. <p style="color:green;" > JIKA YANG SUDAH DOWNLOAD APLIKASI VERSI TERBARU KLIK Android (BETA V.8.0)</p>
+
+      <div class="container">
+
+        <div class="row gy-4">
+
+          <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="100">
+            <div class="service-item item-cyan position-relative">
+              <div class="icon">
+                <i class="bi bi-activity"></i>
+              </div>
+              <a href="http://192.168.88.203/dashboard/UPDATE-ROBOT-8.0/APLIKASI/ANDROID.apk" class="stretched-link">
+                <h3>Android (V.8.0)</h3>
+              </a>
+              <p>Download Untuk Handphone Android (UPDATE 10082024-1051.V.8.0-CODE-9999)</p>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="200">
+            <div class="service-item item-cyan position-relative">
+              <div class="icon">
+                <i class="bi bi-activity"></i>
+              </div>
+              <a href="http://192.168.88.203/dashboard/ROBOT-AI-SYSTEM.80-FINAL-UPDATE/" class="stretched-link">
+                <h3>Android (BETA V.8.0)</h3>
+              </a>
+              <p>Download Untuk Handphone Android Versi BETA (UPDATE 10082024-1051.V.8.0-CODE-9999)</p>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="300">
+            <div class="service-item item-cyan position-relative">
+              <div class="icon">
+                <i class="bi bi-activity"></i>
+              </div>
+              <a href="http://192.168.88.203/dashboard/UPDATE-ROBOT-8.0/APLIKASI/IOS.tar.gz" class="stretched-link">
+                <h3>Iphone IOS (V.8.0)</h3>
+              </a>
+              <p>Download Untuk Handphone Iphone (UPDATE 10082024-1051.V.8.0-CODE-9999)</p>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="400">
+            <div class="service-item item-cyan position-relative">
+              <div class="icon">
+                <i class="bi bi-activity"></i>
+              </div>
+              <a href="http://192.168.88.203/dashboard/UPDATE-ROBOT-8.0/APLIKASI/KOMPUTER.desktop" class="stretched-link">
+                <h3>Windos & Linux</h3>
+              </a>
+              <p>Download Untuk Komputer Windows & Linux (UPDATE 10082024-1051.V.8.0-CODE-9999)</p>
+              <a href="http://192.168.88.203/dashboard/UPDATE-ROBOT-8.0/APLIKASI/KOMPUTER.php" class="stretched-link"></a>
+            </div>
+          </div><!-- End Service Item -->
+        </div>
+
+      </div>
+
+
+
+     
+
+
+    
+    <div class="container copyright text-center mt-4">
+    
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you've purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
+   
+      </div>
+    </div>
+
+  </footer>
+
+  <!-- Scroll Top -->
+  <a href="#" id="" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short">
+  <!-- Preloader -->
+
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+
+  <!-- Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
