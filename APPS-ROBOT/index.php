6881d54e0f1568b3d157a4c1fad5f65b98f1f70e
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2024-08-13 14:49:30 +0700

    Add files via upload

---
+++ APPS-ROBOT/index.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <title>Aplikasi RS. Asura</title>
+    <!-- Stylesheet File -->
+    <link rel="stylesheet" href="assets/css/vendor.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+	  <link href="http://192.168.88.203/dashboard/download.jpeg" rel="icon" type="image/png" />
+<meta http-equiv="refresh" content="100000;url=http://192.168.88.203/dashboard/APPS-ROBOT/"> 
+
+<audio autoplay>
+    
+<source src="http://192.168.88.203/dashboard/AUDIO/HORE.mp3" type="audio/mpeg">
+    </audio>
+</head>
+
+<body>
+
+    <!-- preloader area start -->
+    <div class="preloader" id="preloader">
+        <div class="preloader-inner">
+            <div class="spinner">
+                <div class="dot1"></div>
+                <div class="dot2"></div>
+            </div>
+        </div>
+    </div>
+    <!-- preloader area end -->
+
+    <!-- auto notification start -->
+    <div class="modal fade fade-modal-nitification" id="overlay">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="ba-bill-pay-inner">
+                    <div class="ba-single-bill-pay">
+                        <div class="thumb">
+                            <img src="http://192.168.88.203/dashboard/ROBOT-AI-SYSTEM.80-FINAL-UPDATE/assets/img/ROBOT.gif" alt="img">
+                        </div>
+                        <div class="details">
+                            <h5>AI Robot System 8.0</h5>
+                            <p>Sudah Update</p>
+                        </div>
+                    </div>
+                    <div class="amount-inner">
+                                             
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- auto notification end -->
+
+    <!-- header start -->
+    <div class="header-area" style="background-image: url(assets/img/bg/8.png);">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4 col-3">
+                    <div class="">
+                        <i class=""></i>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-4 text-center">
+                    <a href="index.php" class="logo">
+                    
+                    </a>
+                </div>
+                <div class="col-sm-4 col-5 text-right">
+                    <ul class="header-right">
+                        <li>
+                            <a href="#">
+                                
+                            </a>
+                        </li>
+                        <li>
+                            <a href="notification.php">
+                                <i class="fa fa-bell animated infinite swing"></i>
+                                <span class="badge">0</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="header-user" href="http://192.168.88.203/dashboard/APPS-ROBOT/internet.php"><img src="http://192.168.88.203/dashboard/APPS-ROBOT/USER.png"  alt="img"></a>   <span class="badge">0</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- header end -->
+
+   
+
+ 
+
+    <!-- balance start -->
+    <div class="balance-area pd-top-40 mg-top-50">
+        <div class="container">
+            <div class="balance-area-bg balance-area-bg-home">
+                <div class="balance-title text-center">
+                    <h6>APLIKASI SUPERHERO<br> </h6>
+                </div>
+                <div class="ba-balance-inner text-center" style="background-image: url(assets/img/bg/2.png);">
+                    <div class="icon">
+                        <img src="http://192.168.88.203/dashboard/ROBOT-AI-SYSTEM.80-FINAL-UPDATE/assets/img/ROBOT.gif" alt="img">
+                    </div>
+                    <h5 class="title">Selamat Datang</h5>
+                    <h5 class="amount"></h5>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- balance End -->
+
+    <!-- add balance start -->
+    <div class="add-balance-area pd-top-40">
+        <div class="container">
+            <div class="ba-add-balance-title ba-add-balance-btn">
+                <h5>TAGIHAN INTERNET</h5>
+
+                <i class="fa fa-plus"></i>
+
+   </div>
+  <div class="ba-add-balance-title ba-add-balance-btn" href="https://cektagihan.my.id/">
+                <h5>TAGIHAN PLN</h5>
+                <i class="fa fa-plus"></i>
+
+
+            </div>
+            <div class="ba-add-balance-inner mg-top-40">
+                <div class="row custom-gutters-20">
+                    <div class="col-6">
+                      <a class="btn btn-green ba-add-balance-btn" href="http://192.168.88.203/dashboard/APPS-ROBOT/datapasien.php">Data Pasien <i class="fa fa-arrow-right"></i></a>   
+                    </div>
+                    <div class="col-6">
+                        <a class="btn btn-green ba-add-balance-btn" href="http://192.168.88.203/dashboard/APPS-ROBOT/jumlahpasien.php">Jumlah Pasien <i class="fa fa-arrow-right"></i></a>
+                    </div>
+                    <div class="col-6">
+                        <a class="btn btn-green ba-add-balance-btn" href="http://192.168.88.203/dashboard/APPS-ROBOT/pasienbayar.php">Pasien Bayar <i class="fa fa-arrow-right "></i></a>
+                    </div>
+                    <div class="col-6">
+                        <a class="btn btn-green ba-add-balance-btn" href="http://192.168.88.203/dashboard/APPS-ROBOT/pasienreg.php">Pasien REG<i class="fa fa-arrow-right"></i></a>
+                    </div>
+<div class="col-6">
+                        <a class="btn btn-green ba-add-balance-btn" href="http://192.168.88.203/dashboard/APPS-ROBOT/daftar.php">Daftar Pasien<i class="fa fa-arrow-right"></i></a>
+                    </div>
+<div class="col-6">
+                        <a class="btn btn-green ba-add-balance-btn" href="http://192.168.88.203/dashboard/APPS-ROBOT/cekobat.php">Cek Obat<i class="fa fa-arrow-right"></i></a>
+                    </div>
+<div class="col-6">
+                        <a class="btn btn-green ba-add-balance-btn" href="http://192.168.88.203/dashboard/APPS-ROBOT/ranap.php">Cek Ranap<i class="fa fa-arrow-right"></i></a>
+                    </div>
+<div class="col-6">
+                        <a class="btn btn-green ba-add-balance-btn" href="http://192.168.88.203/dashboard/APPS-ROBOT/e-wallet.php">E-Wallet<i class="fa fa-arrow-right"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="blog-area pd-top-36 pb-2 mg-top-40" style="background-image: url(assets/img/bg/6.png);">
+        <div class="container">
+            <div class="section-title">
+                <h3 class="title">Agenda</h3>
+            
+            </div>
+            <div class="blog-slider owl-carousel">
+                <div class="item">
+                    <div class="single-blog">
+                        <div class="thumb">
+                            <img src="http://192.168.88.203/dashboard/APPS-ROBOT/ARISAN.jpeg" alt="img">
+                        </div>
+                        <div class="details">
+                            <a href="index.php">Arisan Karyawan</a>
+                        </div>
+                    </div>
+                </div>
+
+
+<div class="item">
+                    <div class="single-blog">
+                        <div class="thumb">
+                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/JAT.jpeg" alt="img">
+                        </div>
+                        <div class="details">
+                            <a href="index.php">MOU Limbah B3 PT. JAT</a>
+                        </div>
+                    </div>
+                </div>
+
+
+<div class="item">
+                    <div class="single-blog">
+                        <div class="thumb">
+                            <img src="http://192.168.88.203/dashboard/SISTEM8.0/INHEALTH.jpg" alt="img">
+                        </div>
+                        <div class="details">
+                            <a href="index.php">MOU Asuransi Mandiri Inhelath</a>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+                <div class="item">
+                    <div class="single-blog">
+                        <div class="thumb">
+                            <img src="http://192.168.88.203/dashboard/APPS-ROBOT/AKREDITASI.jpeg" alt="img">
+                        </div>
+                        <div class="details">
+                            <a href="index.php">Persiapan Akreditasi</a>
+
+
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- blog-area End -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="footer-area">
+        <div class="footer-top text-center" style="background-image: url();">
+            <div class="container">
+                
+            </div>
+        </div>
+
+    <!-- Footer Area -->
+    
+            <div class="footer-bottom text-center">
+                <ul>
+                    <li>
+                        <a href="index.php">
+                            <i class="fa fa-home"></i>
+                            <p>Beranda</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="aplikasi.php">
+                            <i class="fa fa-file-text"></i>
+                            <p>Aplikasi</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="upload.php">
+                            <i class="fa fa-plus"></i>
+                            <p>Upload</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="poliklinik.php">
+                            <i class="fa fa-bars"></i>
+                            <p>Poliklinik</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="login.php">
+                            <i class="fa fa-bars"></i>
+                            <p>Login</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <!-- back to top area start -->
+    <div class="back-to-top">
+        <span class="back-top"><i class="fa fa-angle-up"></i></span>
+    </div>
+    <!-- back to top area end -->
+
+    <!-- All Js File here -->
+    <script src="assets/js/vendor.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
